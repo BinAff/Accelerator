cc4eb9374b0a67370d78b68790a52059337d96a9
ArpanKar <ArpanKar@61b5b83d-0e8f-4795-99eb-9aad62fa1a29>
2013-09-17 08:36:25 +0000

    Added with error

---
+++ Vanilla/Source Code/Vanilla/Configuration/Vanilla.Configuration.Lodge.WinForm/Building.Designer.cs
@@ -0,0 +1,230 @@
+﻿namespace AutoTourism.Configuration
+{
+    partial class Building
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.cboBuildingList = new System.Windows.Forms.ComboBox();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.lblName = new System.Windows.Forms.Label();
+            this.txtFloor = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.cboType = new System.Windows.Forms.ComboBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.bttnAddContact = new System.Windows.Forms.Button();
+            this.btnRemove = new System.Windows.Forms.Button();
+            this.lstFloorList = new System.Windows.Forms.ListBox();
+            this.cboFloor = new System.Windows.Forms.ComboBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.errorProvider = new System.Windows.Forms.ErrorProvider(this.components);
+            this.chkDefault = new System.Windows.Forms.CheckBox();
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // cboBuildingList
+            // 
+            this.cboBuildingList.DropDownStyle = System.Windows.Forms.ComboBoxStyle.Simple;
+            this.cboBuildingList.FormattingEnabled = true;
+            this.cboBuildingList.Location = new System.Drawing.Point(8, 40);
+            this.cboBuildingList.Name = "cboBuildingList";
+            this.cboBuildingList.Size = new System.Drawing.Size(174, 241);
+            this.cboBuildingList.TabIndex = 1;
+            this.cboBuildingList.SelectedIndexChanged += new System.EventHandler(this.cboBuildingList_SelectedIndexChanged);
+            // 
+            // txtName
+            // 
+            this.txtName.Location = new System.Drawing.Point(261, 40);
+            this.txtName.MaxLength = 50;
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(164, 20);
+            this.txtName.TabIndex = 3;
+            // 
+            // lblName
+            // 
+            this.lblName.AutoSize = true;
+            this.lblName.Location = new System.Drawing.Point(193, 44);
+            this.lblName.Name = "lblName";
+            this.lblName.Size = new System.Drawing.Size(35, 13);
+            this.lblName.TabIndex = 2;
+            this.lblName.Text = "Name";
+            // 
+            // txtFloor
+            // 
+            this.txtFloor.Location = new System.Drawing.Point(261, 66);
+            this.txtFloor.MaxLength = 3;
+            this.txtFloor.Name = "txtFloor";
+            this.txtFloor.Size = new System.Drawing.Size(40, 20);
+            this.txtFloor.TabIndex = 5;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(193, 70);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(30, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Floor";
+            // 
+            // cboType
+            // 
+            this.cboType.FormattingEnabled = true;
+            this.cboType.Location = new System.Drawing.Point(261, 154);
+            this.cboType.Name = "cboType";
+            this.cboType.Size = new System.Drawing.Size(164, 21);
+            this.cboType.TabIndex = 72;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(193, 160);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(31, 13);
+            this.label6.TabIndex = 71;
+            this.label6.Text = "Type";
+            // 
+            // bttnAddContact
+            // 
+            this.bttnAddContact.Font = new System.Drawing.Font("Arial Narrow", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.bttnAddContact.Location = new System.Drawing.Point(318, 65);
+            this.bttnAddContact.Name = "bttnAddContact";
+            this.bttnAddContact.Size = new System.Drawing.Size(32, 22);
+            this.bttnAddContact.TabIndex = 74;
+            this.bttnAddContact.Text = "  ►";
+            this.bttnAddContact.UseVisualStyleBackColor = true;
+            this.bttnAddContact.Click += new System.EventHandler(this.bttnAddContact_Click);
+            // 
+            // btnRemove
+            // 
+            this.btnRemove.Font = new System.Drawing.Font("Arial Black", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnRemove.Location = new System.Drawing.Point(318, 93);
+            this.btnRemove.Name = "btnRemove";
+            this.btnRemove.Size = new System.Drawing.Size(32, 22);
+            this.btnRemove.TabIndex = 75;
+            this.btnRemove.Text = "X";
+            this.btnRemove.UseVisualStyleBackColor = true;
+            this.btnRemove.Click += new System.EventHandler(this.btnRemove_Click);
+            // 
+            // lstFloorList
+            // 
+            this.lstFloorList.FormattingEnabled = true;
+            this.lstFloorList.Location = new System.Drawing.Point(356, 66);
+            this.lstFloorList.Name = "lstFloorList";
+            this.lstFloorList.SelectionMode = System.Windows.Forms.SelectionMode.MultiSimple;
+            this.lstFloorList.Size = new System.Drawing.Size(69, 82);
+            this.lstFloorList.TabIndex = 76;
+            // 
+            // cboFloor
+            // 
+            this.cboFloor.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cboFloor.FormattingEnabled = true;
+            this.cboFloor.Location = new System.Drawing.Point(261, 181);
+            this.cboFloor.Name = "cboFloor";
+            this.cboFloor.Size = new System.Drawing.Size(61, 21);
+            this.cboFloor.TabIndex = 82;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(193, 184);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(67, 13);
+            this.label3.TabIndex = 81;
+            this.label3.Text = "Default Floor";
+            // 
+            // errorProvider
+            // 
+            this.errorProvider.ContainerControl = this;
+            // 
+            // chkDefault
+            // 
+            this.chkDefault.AutoSize = true;
+            this.chkDefault.Location = new System.Drawing.Point(261, 210);
+            this.chkDefault.Name = "chkDefault";
+            this.chkDefault.Size = new System.Drawing.Size(68, 17);
+            this.chkDefault.TabIndex = 133;
+            this.chkDefault.Text = "IsDefault";
+            this.chkDefault.UseVisualStyleBackColor = true;
+            // 
+            // Building
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(490, 287);
+            this.Controls.Add(this.chkDefault);
+            this.Controls.Add(this.cboBuildingList);
+            this.Controls.Add(this.txtName);
+            this.Controls.Add(this.lblName);
+            this.Controls.Add(this.cboFloor);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.lstFloorList);
+            this.Controls.Add(this.btnRemove);
+            this.Controls.Add(this.bttnAddContact);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtFloor);
+            this.Controls.Add(this.cboType);
+            this.Controls.Add(this.label6);
+            this.Name = "Building";
+            this.Text = "Building Configuration";
+            this.Load += new System.EventHandler(this.Building_Load);
+            this.Controls.SetChildIndex(this.label6, 0);
+            this.Controls.SetChildIndex(this.cboType, 0);
+            this.Controls.SetChildIndex(this.txtFloor, 0);
+            this.Controls.SetChildIndex(this.label1, 0);
+            this.Controls.SetChildIndex(this.bttnAddContact, 0);
+            this.Controls.SetChildIndex(this.btnRemove, 0);
+            this.Controls.SetChildIndex(this.lstFloorList, 0);
+            this.Controls.SetChildIndex(this.label3, 0);
+            this.Controls.SetChildIndex(this.cboFloor, 0);
+            this.Controls.SetChildIndex(this.lblName, 0);
+            this.Controls.SetChildIndex(this.txtName, 0);
+            this.Controls.SetChildIndex(this.cboBuildingList, 0);
+            this.Controls.SetChildIndex(this.chkDefault, 0);
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox cboBuildingList;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.Label lblName;
+        private System.Windows.Forms.TextBox txtFloor;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox cboType;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Button bttnAddContact;
+        private System.Windows.Forms.Button btnRemove;
+        private System.Windows.Forms.ListBox lstFloorList;
+        private System.Windows.Forms.ComboBox cboFloor;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ErrorProvider errorProvider;
+        private System.Windows.Forms.CheckBox chkDefault;
+    }
+}
---
+++ Vanilla/Source Code/Vanilla/Configuration/Vanilla.Configuration.Lodge.WinForm/Building.cs
@@ -0,0 +1,445 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+using System.Windows.Forms;
+
+//using BinAff.Core;
+//using BinAff.Utility;
+
+//using AutoTourism.Presentation.Library;
+
+//using AutoTourism.Facade.Configuration.Building;
+
+namespace AutoTourism.Configuration
+{
+
+    public partial class Building : Form
+    {
+        public enum BuildingStatus
+        { 
+            Open = 10001,
+            Close = 10002
+        }
+
+
+        public Building()
+        {
+            InitializeComponent();
+        }
+
+        //private AutoTourism.Facade.UserManagement.User.Dto userDto = null;
+
+        //public Building(AutoTourism.Facade.UserManagement.User.Dto userDto)
+        //{
+        //    InitializeComponent();
+
+        //    this.userDto = userDto;
+        //    base.IsMinimizeBox = false;
+        //    base.IsOpenButton = true;
+        //    base.IsCloseButton = true;
+        //}
+
+        private void Building_Load(object sender, System.EventArgs e)
+        {
+            LoadForm();
+            Clear();
+        }
+
+        protected override void btnAdd_Click(object sender, System.EventArgs e)
+        {
+            //if (ValidateUser())
+            //{
+            //    Dto buildingDto = new Dto()
+            //    {
+            //        Name = txtName.Text.Trim(),
+            //        Floor = (List<Int32>)lstFloorList.DataSource,
+            //        Type = (AutoTourism.Facade.Configuration.BuildingType.Dto)this.cboType.SelectedItem,
+            //        DefaultFloor = Convert.ToInt32(this.cboFloor.SelectedItem),
+            //        IsDefault = chkDefault.Checked
+            //    };
+
+            //    if (!ValidateUnique(buildingDto) && OverWriteDefaultIfExists(buildingDto))
+            //    {
+            //        IBuilding building = new BuildingServer();
+            //        ReturnObject<Boolean> ret = building.Add(buildingDto);
+
+            //        base.ShowMessage(ret); //Show message     
+            //    }
+            //}
+        }
+
+        protected override void btnChange_Click(object sender, EventArgs e)
+        {
+            //if (this.cboBuildingList.SelectedIndex == -1)
+            //{
+            //    //Show message
+            //    new MessageBox("Please select one building", MessageBox.Type.Alert).ShowDialog(this);
+            //    return;
+            //}
+
+            //if (ValidateUser())
+            //{
+            //    Dto buildingDto = new Dto()
+            //    {
+            //        Id = ((Dto)this.cboBuildingList.SelectedItem).Id,
+            //        Name = txtName.Text.Trim(),
+            //        Floor = (List<Int32>)lstFloorList.DataSource,
+            //        Type = (AutoTourism.Facade.Configuration.BuildingType.Dto)this.cboType.SelectedItem,
+            //        DefaultFloor = Convert.ToInt32(this.cboFloor.SelectedItem),
+            //        IsDefault = chkDefault.Checked,
+            //    };
+
+            //    if (!ValidateUnique(buildingDto) && OverWriteDefaultIfExists(buildingDto))
+            //    {
+            //        IBuilding building = new BuildingServer();
+            //        ReturnObject<Boolean> ret = building.Change(buildingDto);
+
+            //        base.ShowMessage(ret); //Show message                    
+            //    }
+
+            //}
+        }
+
+        protected override void btnDelete_Click(object sender, EventArgs e)
+        {
+            //if (this.cboBuildingList.SelectedIndex == -1)
+            //{
+            //    //Show message
+            //    new MessageBox("Please select one building.", MessageBox.Type.Alert).ShowDialog(this);
+            //    return;
+            //}
+
+            //if (System.Windows.Forms.MessageBox.Show("Are you sure you want to delete the building.?", "Building Delete",
+            //                System.Windows.Forms.MessageBoxButtons.YesNo,
+            //                System.Windows.Forms.MessageBoxIcon.Question)
+            //                  == System.Windows.Forms.DialogResult.Yes)
+            //{
+
+            //    IBuilding building = new BuildingServer();
+            //    ReturnObject<Boolean> ret = building.Delete(new Dto()
+            //    {
+            //        Id = ((Dto)this.cboBuildingList.SelectedItem).Id
+            //    });
+
+            //    base.ShowMessage(ret); //Show message
+            //}
+            //else Clear();
+        }
+
+        protected override void btnRefresh_Click(object sender, EventArgs e)
+        {
+            //LoadForm();
+            //Clear();
+        }
+
+        private void bttnAddContact_Click(object sender, EventArgs e)
+        {
+            //errorProvider.Clear();
+
+            //ReturnObject<List<Int32>> retObj;
+            //if (txtFloor.Text.Trim() != String.Empty)
+            //{
+            //    if (ValidationRule.IsInteger(txtFloor.Text.Trim()))
+            //    {
+            //        retObj = GetFloorList(Convert.ToInt32(txtFloor.Text.Trim()), (List<Int32>)lstFloorList.DataSource);
+            //        if (retObj.HasError())
+            //            errorProvider.SetError(txtFloor, "Entered floor already exists.");
+            //        else
+            //        {
+            //            lstFloorList.DataSource = null;
+            //            lstFloorList.DataSource = retObj.Value;
+            //            lstFloorList.SelectedIndex = -1;
+
+            //            //populate Default floor
+            //            List<Int32> DefaultFloorList = new List<Int32>();
+            //            foreach (Int32 dFloor in retObj.Value)
+            //            {
+            //                DefaultFloorList.Add(dFloor);
+            //            }
+            //            cboFloor.DataSource = null;
+            //            cboFloor.DataSource = DefaultFloorList;
+            //        }
+            //        txtFloor.Text = String.Empty;
+            //    }
+            //    else
+            //        errorProvider.SetError(txtFloor, "Please enter integer values.");
+            //}
+        }
+
+        private void btnRemove_Click(object sender, EventArgs e)
+        {
+            List<Int32> retList = new List<Int32>();
+            List<Int32> FloorList = (List<Int32>)lstFloorList.DataSource;
+            Boolean blnAdd = true;
+            if (FloorList != null && lstFloorList.SelectedItems.Count > 0)
+            {
+                foreach (Int32 iFloor in FloorList)
+                {
+                    foreach (Int32 selFloor in lstFloorList.SelectedItems)
+                    {
+                        if (iFloor == selFloor)
+                        {
+                            blnAdd = false;
+                            break;
+                        }
+                    }
+                    if (blnAdd)
+                        retList.Add(iFloor);
+
+                    blnAdd = true;
+                }
+
+                lstFloorList.DataSource = null;
+                lstFloorList.DataSource = retList;
+                lstFloorList.SelectedIndex = -1;
+
+                //populate Default floor
+                List<Int32> DefaultFloorList = new List<Int32>();
+                foreach (Int32 dFloor in retList)
+                {
+                    DefaultFloorList.Add(dFloor);
+                }
+                cboFloor.DataSource = null;
+                cboFloor.DataSource = DefaultFloorList;
+            }
+        }
+
+        private void cboBuildingList_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            //if (this.cboBuildingList.SelectedIndex != -1)
+            //{
+            //    Dto data = (Dto)this.cboBuildingList.SelectedItem;
+            //    txtName.Text = data.Name;
+            //    chkDefault.Checked = data.IsDefault;
+
+            //    lstFloorList.DataSource = data.Floor;
+            //    lstFloorList.SelectedIndex = -1;
+
+            //    for (int i = 0; i < cboType.Items.Count; i++)
+            //    {
+            //        if (data.Type.Id == ((AutoTourism.Facade.Configuration.BuildingType.Dto)cboType.Items[i]).Id)
+            //        {
+            //            cboType.SelectedIndex = i;
+            //            break;
+            //        }
+            //    }
+
+            //    //populate Default floor
+            //    if (data.Floor != null)
+            //    {
+            //        List<Int32> DefaultFloorList = new List<Int32>();
+            //        foreach (Int32 dFloor in data.Floor)
+            //        {
+            //            DefaultFloorList.Add(dFloor);
+            //        }
+            //        cboFloor.DataSource = null;
+            //        cboFloor.DataSource = DefaultFloorList;
+            //        cboFloor.SelectedItem = data.DefaultFloor;
+            //    }
+            //}
+        }
+
+        protected override void btnClose_Click(object sender, EventArgs e)
+        {
+            //if (this.cboBuildingList.SelectedIndex == -1)
+            //{
+            //    //Show message
+            //    new MessageBox("Please select one building.", MessageBox.Type.Alert).ShowDialog(this);
+            //    return;
+            //}
+
+            //Facade.Configuration.Building.Dto dto = (Facade.Configuration.Building.Dto)this.cboBuildingList.SelectedItem;
+            //if (dto.Status.Id == Convert.ToInt64(BuildingStatus.Close))
+            //{
+            //    ReturnObject<Boolean> ret = new ReturnObject<Boolean>() { 
+            //        Value = true,
+            //        MessageList = new List<Message>(),
+            //    };
+            //    ret.MessageList.Add(new Message("Building is already closed.", Message.Type.Information));
+            //    base.ShowMessage(ret);
+            //}
+            //else
+            //{
+            //    new ReasonDialog(Convert.ToString(EnumDefination.FormName.Building), this.userDto, dto.Id,0).ShowDialog(this);
+
+            //    LoadForm();
+            //    Clear();
+            //}
+        }
+
+        protected override void btnOpen_Click(object sender, EventArgs e)
+        {
+            //if (this.cboBuildingList.SelectedIndex == -1)
+            //{
+            //    //Show message
+            //    new MessageBox("Please select one building.", MessageBox.Type.Alert).ShowDialog(this);
+            //    return;
+            //}
+
+            //if (((Dto)this.cboBuildingList.SelectedItem).Status.Id != Convert.ToInt64(BuildingStatus.Close))
+            //{
+            //    new MessageBox("Unable to open building. Only closed buildings can be opened", MessageBox.Type.Error).ShowDialog(this);//Show message
+            //    return;
+            //}
+           
+            //Facade.Configuration.Building.Dto dto = (Facade.Configuration.Building.Dto)this.cboBuildingList.SelectedItem;
+            //if (dto.Status.Id == Convert.ToInt64(BuildingStatus.Open))
+            //{
+            //    ReturnObject<Boolean> ret = new ReturnObject<Boolean>()
+            //    {
+            //        Value = true,
+            //        MessageList = new List<Message>(),
+            //    };
+            //    ret.MessageList.Add(new Message("Building is already open.", Message.Type.Information));
+            //    base.ShowMessage(ret);
+            //}
+            //else
+            //{
+            //    IBuilding building = new BuildingServer();
+            //    ReturnObject<Boolean> ret = building.Open(new Dto()
+            //    {
+            //        Id = ((Dto)this.cboBuildingList.SelectedItem).Id,
+            //    });
+
+            //    base.ShowMessage(ret);
+            //    LoadForm();
+            //    Clear();
+
+            //}
+        }
+
+        protected override void LoadForm()
+        {
+            //IBuilding building = new BuildingServer();
+            //ReturnObject<FormDto> ret = building.LoadForm();
+
+            ////Populate Floor List
+            //this.cboBuildingList.DataSource = ret.Value.BuildingList;
+            //this.cboBuildingList.DisplayMember = "Name";
+            //this.cboBuildingList.ValueMember = "Id";
+            //this.cboBuildingList.SelectedIndex = -1;
+
+            ////populate Type List
+            //this.cboType.DataSource = ret.Value.TypeList;
+            //this.cboType.DisplayMember = "Name";
+            //this.cboType.ValueMember = "Id";
+        }
+
+        protected override void Clear()
+        {
+            this.txtName.Text = String.Empty;
+            this.txtFloor.Text = String.Empty;
+            this.lstFloorList.DataSource = null;
+            this.cboFloor.DataSource = null;
+            this.chkDefault.Checked = false;
+        }
+
+        //private Boolean ValidateUnique(Dto data)
+        //{
+        //    List<Dto> list = (List<Dto>)this.cboBuildingList.DataSource;
+        //    foreach (Dto dto in list)
+        //    {
+        //        if (dto.Name.ToUpper() == txtName.Text.Trim().ToUpper() && dto.Id != data.Id)
+        //        {
+        //            //Show message // TO DO :: Need to change
+        //            new MessageBox("Building already exists.", MessageBox.Type.Information).ShowDialog(this);
+        //            return true;
+        //        }
+        //    }
+        //    return false;
+        //}
+
+        //private Boolean OverWriteDefaultIfExists(Dto data)
+        //{
+        //    if (chkDefault.Checked)
+        //    {
+        //        List<Dto> list = (List<Dto>)this.cboBuildingList.DataSource;
+        //        foreach (Dto dto in list)
+        //        {
+        //            if (dto.Id != data.Id && dto.IsDefault)
+        //            {
+        //                //Show message
+        //                System.Windows.Forms.DialogResult result = System.Windows.Forms.MessageBox.Show("Default already exists. Do you want to overwrite.", "Alert", System.Windows.Forms.MessageBoxButtons.YesNo, System.Windows.Forms.MessageBoxIcon.Question);
+        //                if (result == System.Windows.Forms.DialogResult.Yes)
+        //                    return true;
+        //                else
+        //                    return false;
+        //            }
+        //        }
+
+        //        if (data.IsDefault) return true;
+
+        //        return false;
+        //    }
+        //    return true;
+        //}
+
+        private Boolean ValidateUser()
+        {
+            Boolean retVal = true;
+            errorProvider.Clear();
+
+            if (String.IsNullOrEmpty(txtName.Text.Trim()))
+            {
+                errorProvider.SetError(txtName, "Please enter building name.");
+                txtName.Focus();
+                return false;
+            }
+            else if (!(new Regex(@"^[a-zA-Z_0-9 ]*$").IsMatch(txtName.Text.Trim())))
+            {
+                errorProvider.SetError(txtName, "Entered " + txtName.Text + " is Invalid.");
+                txtName.Focus();
+                return false;
+            }
+            else if (lstFloorList.Items.Count == 0)
+            {
+                errorProvider.SetError(lstFloorList, "Please enter floor(s).");
+                txtFloor.Focus();
+                return false;
+            }
+            else if (this.cboType.DataSource == null)
+            {
+                errorProvider.SetError(cboType, "Please select building type.");
+                return false;
+            }
+
+            return retVal;
+        }
+
+        //private ReturnObject<List<Int32>> GetFloorList(Int32 val, List<Int32> floor)
+        //{
+        //    ReturnObject<List<Int32>> retObj = new ReturnObject<List<int>>()
+        //    {
+        //        Value = new List<Int32>()
+        //    };
+
+        //    if (floor == null || floor.Count == 0)
+        //        retObj.Value.Add(val);
+        //    else
+        //    {
+        //        foreach (Int32 i in floor)
+        //        {
+        //            if (i == val)
+        //            {
+        //                retObj.Value = floor;
+
+        //                retObj.MessageList = new List<BinAff.Core.Message>();
+        //                retObj.MessageList.Add(new BinAff.Core.Message()
+        //                {
+        //                    Description = "Duplicate",
+        //                    Category = BinAff.Core.Message.Type.Error
+        //                });
+        //                return retObj;
+        //            }
+        //        }
+        //        floor.Add(val);
+        //        retObj.Value = floor;
+        //    }
+
+        //    return retObj;
+        //}
+
+    }
+
+}
