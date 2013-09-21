1afa17d1f46b266be19b68d7d54ecfb271f178d2
ArpanKar <ArpanKar@61b5b83d-0e8f-4795-99eb-9aad62fa1a29>
2013-09-21 15:53:47 +0000

    Change data type from Varchar to Char

--- Crystal/Source Code/Crystal/Navigator/Rule/DBScript.sql
+++ Crystal/Source Code/Crystal/Navigator/Rule/DBScript.sql
@@ -1,27 +1,40 @@
-﻿IF NOT EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].[NavigatorRule]') AND type in (N'U'))
-CREATE TABLE [dbo].[NavigatorRule](
-	[Id] Numeric(10,0) NOT NULL,
-	[ModuleSeperator] [varchar](1) NULL,
-	[PathSeperator] [varchar](1) NULL,
-	CONSTRAINT [PK_NavigatorRule] PRIMARY KEY CLUSTERED 
-	(
-		[Id] ASC
-	)WITH (PAD_INDEX  = OFF, STATISTICS_NORECOMPUTE  = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS  = ON, ALLOW_PAGE_LOCKS  = ON)
+﻿USE [AutoTourism]
+GO
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+SET ANSI_PADDING ON
+GO
+
+IF NOT EXISTS (SELECT * FROM sys.schemas WHERE name = 'Navigator')
+	EXEC('CREATE SCHEMA Navigator AUTHORIZATION dbo');
+GO
+
+IF NOT EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[Navigator].[Rule]') AND type in (N'U'))
+CREATE TABLE [Navigator].[Rule](
+	[Id] [numeric](10, 0) IDENTITY(1,1) NOT NULL,
+	[ModuleSeperator] [char](1) NOT NULL,
+	[PathSeperator] [char](1) NOT NULL,
+ CONSTRAINT [PK_Rule] PRIMARY KEY CLUSTERED 
+(
+	[Id] ASC
+)WITH (PAD_INDEX  = OFF, STATISTICS_NORECOMPUTE  = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS  = ON, ALLOW_PAGE_LOCKS  = ON) ON [PRIMARY]
 ) ON [PRIMARY]
 GO
 
 /*************C*************/
-IF NOT EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].[NavigatorRuleInsert]') AND type in (N'P', N'PC'))
+IF NOT EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[Navigator].[RuleInsert]') AND type in (N'P', N'PC'))
 BEGIN
-	EXEC dbo.sp_executesql @statement = N'CREATE PROCEDURE [dbo].[NavigatorRuleInsert]
+	EXEC dbo.sp_executesql @statement = N'CREATE PROCEDURE [Navigator].[RuleInsert]
 	(  
-		@ModuleSeperator Varchar(1),
-		@PathSeperator Varchar(1),
+		@ModuleSeperator Char(1),
+		@PathSeperator Char(1),
 		@Id  Numeric(10,0) OUTPUT
 	)
 	AS
 	BEGIN
-			INSERT INTO NavigatorRule(Id, ModuleSeperator, PathSeperator)
+			INSERT INTO [Navigator].[Rule](Id, ModuleSeperator, PathSeperator)
 			VALUES(1, @ModuleSeperator, @PathSeperator)
     
 			SET @Id = 1
@@ -31,9 +44,9 @@ END
 GO
 
 /*************R*************/
-IF NOT EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].[NavigatorRuleRead]') AND type in (N'P', N'PC'))
+IF NOT EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[Navigator].[RuleRead]') AND type in (N'P', N'PC'))
 BEGIN
-	EXEC dbo.sp_executesql @statement = N'CREATE PROCEDURE [dbo].[NavigatorRuleRead]
+	EXEC dbo.sp_executesql @statement = N'CREATE PROCEDURE [Navigator].[RuleRead]
 	(  
 	   @Id Numeric(10,0)  
 	)  
@@ -41,23 +54,23 @@ BEGIN
 	BEGIN
 		SELECT
 			ModuleSeperator, PathSeperator
-		FROM NavigatorRule
+		FROM [Navigator].[Rule]
 	END
 	'
 END
 GO
 
 /************U**************/
-IF NOT EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].[NavigatorRuleUpdate]') AND type in (N'P', N'PC'))
+IF NOT EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[Navigator].[RuleUpdate]') AND type in (N'P', N'PC'))
 BEGIN
-	EXEC dbo.sp_executesql @statement = N'CREATE PROCEDURE [dbo].[NavigatorRuleUpdate]
+	EXEC dbo.sp_executesql @statement = N'CREATE PROCEDURE [Navigator].[RuleUpdate]
 	(  
-		@ModuleSeperator Varchar(1),
-		@PathSeperator Varchar(1)
+		@ModuleSeperator Char(1),
+		@PathSeperator Char(1)
 	)
 	AS
 	BEGIN
-		UPDATE NavigatorRule
+		UPDATE [Navigator].[Rule]
 		SET ModuleSeperator = @ModuleSeperator,
 			PathSeperator = @PathSeperator
 	END
