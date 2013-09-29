4a45c65820f53a2b75fd19f1aa206797ba2c0111
biraj.dhekial@binaryaffairs.com <biraj.dhekial@binaryaffairs.com@61b5b83d-0e8f-4795-99eb-9aad62fa1a29>
2013-09-29 09:17:03 +0000

    Adding project after alteration by Arpan

---
+++ AutoTourism/Source Code/AutoTourism/Lodge/AutoTourism.Lodge.Facade/FormDto.cs
@@ -0,0 +1,16 @@
+﻿using BinAff.Core;
+using System.Collections.Generic;
+
+namespace AutoTourism.Lodge.Facade
+{
+
+    public class FormDto : BinAff.Facade.Library.FormDto
+    {
+
+        public Dto Lodge { get; set; }
+        public List<Dto> LodgeList { get; set; }
+        public List<Table> StateList { get; set; }
+
+    }
+
+}
