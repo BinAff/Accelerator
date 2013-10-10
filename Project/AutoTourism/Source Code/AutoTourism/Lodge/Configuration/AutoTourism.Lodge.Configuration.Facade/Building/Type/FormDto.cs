3c304dbf09d7f3ce978ea278113b3902da67376c
ArpanKar <ArpanKar@61b5b83d-0e8f-4795-99eb-9aad62fa1a29>
2013-10-10 04:41:53 +0000

    Implementing Facade framework in Building Type

--- Project/AutoTourism/Source Code/AutoTourism/Lodge/Configuration/AutoTourism.Lodge.Configuration.Facade/Building/Type/FormDto.cs
+++ Project/AutoTourism/Source Code/AutoTourism/Lodge/Configuration/AutoTourism.Lodge.Configuration.Facade/Building/Type/FormDto.cs
@@ -6,8 +6,8 @@ namespace AutoTourism.Lodge.Configuration.Facade.Building.Type
     public class FormDto : BinAff.Facade.Library.FormDto
     {
 
-        public Dto BuildingType { get; set; }
-        public List<Dto> BuildingTypeList { get; set; }
+        public Dto Dto { get; set; }
+        public List<Dto> DtoList { get; set; }
 
     }
 
