fc406da3b133f1a9ecfc7dbae43a2f874fd96e53
ArpanKar <ArpanKar@61b5b83d-0e8f-4795-99eb-9aad62fa1a29>
2013-10-10 04:41:53 +0000

    Implementing Facade framework in Building Type

--- Project/AutoTourism/Source Code/AutoTourism/Lodge/Configuration/AutoTourism.Lodge.Configuration.Facade/Building/Type/Server.cs
+++ Project/AutoTourism/Source Code/AutoTourism/Lodge/Configuration/AutoTourism.Lodge.Configuration.Facade/Building/Type/Server.cs
@@ -1,13 +1,15 @@
-﻿using BinAff.Core;
-using BinAff.Core.Observer;
-using System;
+﻿using System;
 using System.Collections.Generic;
+
+using BinAff.Core;
+using BinAff.Core.Observer;
+
 using CrystalComponent = Crystal.Lodge.Component.Building.Type;
 
 namespace AutoTourism.Lodge.Configuration.Facade.Building.Type
 {
 
-    public class Server : BinAff.Facade.Library.Server, IBuildingType
+    public class Server : BinAff.Facade.Library.Server
     {
 
         public Server(FormDto formDto)
@@ -15,41 +17,6 @@ namespace AutoTourism.Lodge.Configuration.Facade.Building.Type
         {
             this.FormDto = formDto;
         }
-
-        #region IBuildingType
-        ReturnObject<Boolean> IBuildingType.Delete(Dto dto)
-        {
-            ////Register Observers
-            //Crystal.Lodge.Configuration.Observer.BuildingType buildingType = new Crystal.Lodge.Configuration.Observer.BuildingType();
-
-            //BinAff.Core.ICrud crud = buildingType.RegisterObserver(new Crystal.Lodge.Configuration.Building.Type.Data
-            //{
-            //    Id = dto.Id
-            //});
-
-            //return crud.Delete();
-            return null;
-            
-        }
-
-        ReturnObject<Dto> IBuildingType.Read(Dto dto)
-        {
-            ICrud crud = new CrystalComponent.Server(new CrystalComponent.Data
-            {
-                Id = dto.Id
-            });
-            ReturnObject<BinAff.Core.Data> data = crud.Read();
-            return new BinAff.Core.ReturnObject<Dto>
-            {
-                Value = new Dto
-                {
-                    Id = data.Value.Id,
-                    Name = ((CrystalComponent.Data)data.Value).Name
-                }
-            };
-        }
-
-        #endregion
 
         public override void LoadForm()
         {
@@ -60,7 +27,7 @@ namespace AutoTourism.Lodge.Configuration.Facade.Building.Type
             FormDto formDto = this.FormDto as FormDto;
             foreach (CrystalComponent.Data data in dataList.Value)
             {
-                formDto.BuildingTypeList.Add(this.Convert(data) as Dto);
+                formDto.DtoList.Add(this.Convert(data) as Dto);
             }
         }
 
@@ -78,12 +45,24 @@ namespace AutoTourism.Lodge.Configuration.Facade.Building.Type
         {
             CrystalComponent.Server crud = new CrystalComponent.Server(new CrystalComponent.Data
             {
-                Id = (this.FormDto as FormDto).BuildingType.Id
+                Id = (this.FormDto as FormDto).Dto.Id
             });
             (new Crystal.Lodge.Observer.RoomCategory() as IRegistrar).Register(crud); //Register Observers
 
             ReturnObject<bool> ret = (crud as ICrud).Delete();
             this.DisplayMessageList = ret.GetMessage((this.IsError = ret.HasError()) ? Message.Type.Error : Message.Type.Information);
+        }
+
+        public override void Read()
+        {
+            FormDto formDto = this.FormDto as FormDto;
+            CrystalComponent.Data data = new CrystalComponent.Data
+            {
+                Id = formDto.Dto.Id
+            };
+            ReturnObject<BinAff.Core.Data> ret = (new CrystalComponent.Server(data) as ICrud).Read();
+            this.DisplayMessageList = ret.GetMessage((this.IsError = ret.HasError()) ? Message.Type.Error : Message.Type.Information);
+            formDto.Dto = this.Convert(data) as Dto;
         }
 
         protected override BinAff.Facade.Library.Dto Convert(Data data)
@@ -108,7 +87,7 @@ namespace AutoTourism.Lodge.Configuration.Facade.Building.Type
 
         private void Save()
         {
-            Dto dto = (this.FormDto as FormDto).BuildingType;
+            Dto dto = (this.FormDto as FormDto).Dto;
             ICrud crud = new CrystalComponent.Server(this.Convert(dto) as CrystalComponent.Data);
             ReturnObject<Boolean> ret = crud.Save();
 
