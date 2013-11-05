64186691be4cb1ff39428b1635b5129d11fbcf40
BirajDhekial <BirajDhekial@61b5b83d-0e8f-4795-99eb-9aad62fa1a29>
2013-11-05 08:33:53 +0000

    Added Lodge Dto to remove dependency with the Lodge facade

---
+++ Project/AutoTourism/Source Code/AutoTourism/Customer/AutoTourism.Customer.Facade/Lodge/Reservation/Dto.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace AutoTourism.Customer.Facade.Lodge.Reservation
+{
+    public class Dto : BinAff.Facade.Library.Dto
+    {        
+        public DateTime BookingDate { get; set; }
+        public DateTime BookingFrom { get; set; }
+        public Int32 NoOfDays { get; set; }
+        public Int32 NoOfPersons { get; set; }
+        public Int32 NoOfRooms { get; set; }
+        public Decimal Advance { get; set; }
+        public Int64 BookingStatusId { get; set; }
+
+        public List<Room.Dto> RoomList { get; set; }
+        //public AutoTourism.Facade.CustomerManagement.Dto Customer { get; set; }
+    }
+}
---
+++ Project/AutoTourism/Source Code/AutoTourism/Customer/AutoTourism.Customer.Facade/Lodge/Room/Dto.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+using BinAff.Core;
+
+namespace AutoTourism.Customer.Facade.Lodge.Room
+{
+    public class Dto : BinAff.Facade.Library.Dto
+    {
+        public String Number { get; set; }
+        public String Name { get; set; }
+        public String Description { get; set; }
+        public Table Category { get; set; }
+        public Table Type { get; set; }
+        public Building.Dto Building { get; set; }
+        public Table Floor { get; set; }
+        public Boolean IsAirconditioned { get; set; }
+        public Int64 StatusId { get; set; }
+        public List<Image.Dto> ImageList { get; set; }        
+    }
+}
