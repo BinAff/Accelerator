1e61c58557e11f18049772ed7c77c0062887f734
ArpanKar <ArpanKar@61b5b83d-0e8f-4795-99eb-9aad62fa1a29>
2013-12-03 05:02:29 +0000

    Attaching all forms with lodge config and creating new user control for configuration

---
+++ Project/Vanilla/Source Code/Vanilla/Navigator/WinForm/Configuration.Designer.cs
@@ -0,0 +1,81 @@
+﻿namespace Vanilla.Navigator.WinForm
+{
+    partial class Configuration
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pnlMain = new System.Windows.Forms.SplitContainer();
+            this.lsvConfiguration = new System.Windows.Forms.ListView();
+            ((System.ComponentModel.ISupportInitialize)(this.pnlMain)).BeginInit();
+            this.pnlMain.Panel2.SuspendLayout();
+            this.pnlMain.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // pnlMain
+            // 
+            this.pnlMain.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pnlMain.FixedPanel = System.Windows.Forms.FixedPanel.Panel1;
+            this.pnlMain.Location = new System.Drawing.Point(0, 0);
+            this.pnlMain.Name = "pnlMain";
+            // 
+            // pnlMain.Panel2
+            // 
+            this.pnlMain.Panel2.Controls.Add(this.lsvConfiguration);
+            this.pnlMain.Size = new System.Drawing.Size(404, 274);
+            this.pnlMain.SplitterDistance = 215;
+            this.pnlMain.TabIndex = 11;
+            // 
+            // lsvConfiguration
+            // 
+            this.lsvConfiguration.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.lsvConfiguration.Location = new System.Drawing.Point(0, 0);
+            this.lsvConfiguration.Name = "lsvConfiguration";
+            this.lsvConfiguration.Size = new System.Drawing.Size(185, 274);
+            this.lsvConfiguration.TabIndex = 0;
+            this.lsvConfiguration.UseCompatibleStateImageBehavior = false;
+            this.lsvConfiguration.DoubleClick += new System.EventHandler(this.lsvConfiguration_DoubleClick);
+            // 
+            // Configuration
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.pnlMain);
+            this.Name = "Configuration";
+            this.Size = new System.Drawing.Size(404, 274);
+            this.pnlMain.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pnlMain)).EndInit();
+            this.pnlMain.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.SplitContainer pnlMain;
+        private System.Windows.Forms.ListView lsvConfiguration;
+    }
+}
---
+++ Project/Vanilla/Source Code/Vanilla/Navigator/WinForm/Configuration.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Vanilla.Navigator.WinForm
+{
+
+    public partial class Configuration : UserControl
+    {
+
+        public Configuration()
+        {
+            InitializeComponent();
+        }
+
+        internal void PopulateModuleForConfiguration()
+        {
+            //This should be dynamic
+            Button btnLodge = new Button
+            {
+                Text = "Lodge",
+                Visible = true,
+                Height = 30,
+                Width = this.pnlMain.Panel1.Width,
+                Dock = DockStyle.Top,
+            };
+            this.pnlMain.Panel1.Controls.Add(btnLodge);
+            btnLodge.Click += btnLodge_Click;
+            //
+
+            Button btnGeneral = new Button
+            {
+                Text = "General",
+                Visible = true,
+                Height = 30,
+                Width = this.pnlMain.Panel1.Width,
+                Dock = DockStyle.Top,
+            };
+            this.pnlMain.Panel1.Controls.Add(btnGeneral);
+            btnGeneral.Click += btnGeneral_Click;
+        }
+
+        void btnGeneral_Click(object sender, EventArgs e)
+        {
+            this.lsvConfiguration.Items.Clear();
+            this.lsvConfiguration.Items.Add(new ListViewItem("Customer"));
+            this.lsvConfiguration.Items.Add(new ListViewItem("User"));
+            this.lsvConfiguration.Items.Add(new ListViewItem("Config Manager")
+            {
+                Tag = new Vanilla.Configuration.WinForm.ConfigManager
+                {
+                    TopLevel = true,
+                }
+            });
+        }
+
+        private void lsvConfiguration_DoubleClick(object sender, EventArgs e)
+        {
+            if ((sender as ListView).FocusedItem.Tag != null)
+            {
+                ((sender as ListView).FocusedItem.Tag as Form).ShowDialog();
+            }
+        }
+
+        void btnLodge_Click(object sender, EventArgs e)
+        {
+            this.lsvConfiguration.Items.Clear();
+
+            //Currently Hard coding
+            this.lsvConfiguration.Items.Add(new ListViewItem("Lodge")
+            {
+                Tag = this.InstantiateObject("AutoTourism.Lodge.WinForm.Lodge", "AutoTourism.Lodge.WinForm")
+            }); 
+            
+            this.lsvConfiguration.Items.Add(new ListViewItem("General Configuration")
+            {
+                Tag = this.InstantiateObject("AutoTourism.Lodge.Configuration.WinForm.ConfigManager", "AutoTourism.Lodge.Configuration.WinForm")
+            });
+
+            this.lsvConfiguration.Items.Add(new ListViewItem("Building")
+            {
+                Tag = this.InstantiateObject("AutoTourism.Lodge.Configuration.WinForm.Building", "AutoTourism.Lodge.Configuration.WinForm")
+            });
+
+            this.lsvConfiguration.Items.Add(new ListViewItem("Room")
+            {
+                Tag = this.InstantiateObject("AutoTourism.Lodge.Configuration.WinForm.Room", "AutoTourism.Lodge.Configuration.WinForm")
+            });
+        }
+
+        private Form InstantiateObject(String dataType, String assembly)
+        {
+            Type type = Type.GetType(dataType + ", " + assembly, true);
+            return Activator.CreateInstance(type) as Form;
+        }
+
+    }
+}
