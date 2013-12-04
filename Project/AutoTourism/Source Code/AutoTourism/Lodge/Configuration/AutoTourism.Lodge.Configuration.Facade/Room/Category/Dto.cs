3232d92bf1514756b869a062d311bbddbb93abf1
ArpanKar <ArpanKar@61b5b83d-0e8f-4795-99eb-9aad62fa1a29>
2013-12-04 04:10:25 +0000

    Room category added in configuration

--- Project/AutoTourism/Source Code/AutoTourism/Lodge/Configuration/AutoTourism.Lodge.Configuration.Facade/Room/Category/Dto.cs
+++ Project/AutoTourism/Source Code/AutoTourism/Lodge/Configuration/AutoTourism.Lodge.Configuration.Facade/Room/Category/Dto.cs
@@ -3,10 +3,9 @@
 namespace AutoTourism.Lodge.Configuration.Facade.Room.Category
 {
 
-    public class Dto
+    public class Dto : BinAff.Facade.Library.Dto
     {
 
-        public Int64 Id { get; set; }
         public String Name { get; set; }
 
     }
