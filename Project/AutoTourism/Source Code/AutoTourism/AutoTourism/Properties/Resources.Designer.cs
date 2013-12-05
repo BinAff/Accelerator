02473138eba37ed4a0ae356473d6f42fd3ae1772
ArpanKar <ArpanKar@61b5b83d-0e8f-4795-99eb-9aad62fa1a29>
2013-12-05 09:07:37 +0000

    Adding splash screen, but it will not start now

--- Project/AutoTourism/Source Code/AutoTourism/AutoTourism/Properties/Resources.Designer.cs
+++ Project/AutoTourism/Source Code/AutoTourism/AutoTourism/Properties/Resources.Designer.cs
@@ -59,5 +59,25 @@ namespace AutoTourism.Properties {
                 resourceCulture = value;
             }
         }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap Progress {
+            get {
+                object obj = ResourceManager.GetObject("Progress", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap Splash {
+            get {
+                object obj = ResourceManager.GetObject("Splash", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
     }
 }
---
+++ Project/AutoTourism/Source Code/AutoTourism/AutoTourism/Splash.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Windows.Forms;
+using System.Threading;
+using System.Configuration;
+
+//using AutoTourism.Presentation.Cache;
+
+namespace AutoTourism.Presentation.Guardian
+{
+
+    public partial class Splash : Form
+    {
+
+        private Int32 TimerCount = 0;
+
+        public Splash()
+        {
+            InitializeComponent();
+            this.timer.Start();
+        }
+
+        private void Splash_Click(object sender, EventArgs e)
+        {
+            this.Close();
+            new Thread(new ThreadStart(
+                delegate()
+                {
+                    Type type = Type.GetType(
+                        ConfigurationManager.AppSettings["StartUpAssembly"] + "." +
+                        ConfigurationManager.AppSettings["StartUpClass"] + ", " +
+                        ConfigurationManager.AppSettings["StartUpAssembly"], true);
+                    Application.Run((Form)Activator.CreateInstance(type));
+                })
+            ).Start();
+        }
+
+        private void timer_Tick(object sender, EventArgs e)
+        {
+            //Thread loadRule = new Thread(new ThreadStart(
+            //    delegate()
+            //    {
+            //        //GlobalData.ReadVariables();
+            //    })
+            //);
+            //loadRule.Start();
+
+            //if (TimerCount++ == 10)
+            //{
+            //    while (loadRule.ThreadState == ThreadState.Stopped) 
+            //        loadRule.Abort();
+            //    this.Splash_Click(sender, e);
+            //}
+
+        }
+
+        private void lnkWebsite_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            System.Diagnostics.Process.Start("http://www.binaryaffairs.com");
+        }
+
+    }
+
+}
