7dd96d5191feca7760b17b8f5588696b1ea25629
BirajDhekial <BirajDhekial@61b5b83d-0e8f-4795-99eb-9aad62fa1a29>
2013-12-07 11:20:33 +0000

    Added context menu items and Implemented sorting on listView column Header click

--- Project/Vanilla/Source Code/Vanilla/Navigator/WinForm/Container.cs
+++ Project/Vanilla/Source Code/Vanilla/Navigator/WinForm/Container.cs
@@ -23,6 +23,7 @@ namespace Vanilla.Navigator.WinForm
         private String selectedNodePath;
         private TreeNode editNode;
         private Boolean isCutAction;
+        private ListViewColumnSorter lvwColumnSorter;
 
         public Container()
         {
@@ -105,6 +106,9 @@ namespace Vanilla.Navigator.WinForm
             this.lsvContainer.Columns.Add("Created At", 115);
             this.lsvContainer.Columns.Add("Modified By", 200);
             this.lsvContainer.Columns.Add("Modified At", 115);
+
+            lvwColumnSorter = new ListViewColumnSorter();
+            this.lsvContainer.ListViewItemSorter = lvwColumnSorter;
         }
 
         private void LoadModules(String currentTab)
@@ -1326,6 +1330,165 @@ namespace Vanilla.Navigator.WinForm
             }
         }
 
+        private void lsvContainer_ColumnClick(object sender, ColumnClickEventArgs e)
+        {
+            //clear sort character from column caption
+            for (int i = 0; i < lsvContainer.Columns.Count; i++)
+                lsvContainer.Columns[i].Text = lsvContainer.Columns[i].Text.Replace("v", "").Replace("^", "").Trim();
+
+            this.SortListView(e.Column);
+            
+        }
+
+        private void SortListView(String columnHeaderCaption)
+        {  
+            for (int i = 0; i < lsvContainer.Columns.Count; i++)
+            {
+                if (lsvContainer.Columns[i].Text == columnHeaderCaption)
+                {
+                    this.SortListView(i);
+                    break;
+                }
+            }
+            
+        }
+
+        private void SortListView(Int32 columnIndex)
+        {
+            //clear sort character from column caption
+            for (int i = 0; i < lsvContainer.Columns.Count; i++)
+                lsvContainer.Columns[i].Text = lsvContainer.Columns[i].Text.Replace("v", "").Replace("^", "").Trim();
+            
+
+            //Determine if clicked column is already the column that is being sorted.
+            if (columnIndex == lvwColumnSorter.SortColumn)
+            {
+                // Reverse the current sort direction for this column.
+                if (lvwColumnSorter.Order == SortOrder.Ascending)
+                {
+                    lvwColumnSorter.Order = SortOrder.Descending;
+                    lsvContainer.Columns[lvwColumnSorter.SortColumn].Text += "     ^";
+                }
+                else
+                {
+                    lvwColumnSorter.Order = SortOrder.Ascending;
+                    lsvContainer.Columns[lvwColumnSorter.SortColumn].Text += "     v";
+                }
+            }
+            else
+            {
+                // Set the column number that is to be sorted; default to ascending.
+                lvwColumnSorter.SortColumn = columnIndex;
+                lvwColumnSorter.Order = SortOrder.Ascending;
+                lsvContainer.Columns[lvwColumnSorter.SortColumn].Text += "     v";
+            }
+
+            // Perform the sort with these new sort options.
+            this.lsvContainer.Sort();
+        }
+
     }
+   
+    /// <summary>
+    /// This class is an implementation of the 'IComparer' interface.
+    /// </summary>
+    public class ListViewColumnSorter : IComparer
+    {
+        /// <summary>
+        /// Specifies the column to be sorted
+        /// </summary>
+        private int ColumnToSort;
+        /// <summary>
+        /// Specifies the order in which to sort (i.e. 'Ascending').
+        /// </summary>
+        private SortOrder OrderOfSort;
+        /// <summary>
+        /// Case insensitive comparer object
+        /// </summary>
+        private CaseInsensitiveComparer ObjectCompare;
+
+        /// <summary>
+        /// Class constructor.  Initializes various elements
+        /// </summary>
+        public ListViewColumnSorter()
+        {
+            // Initialize the column to '0'
+            ColumnToSort = 0;
+
+            // Initialize the sort order to 'none'
+            OrderOfSort = SortOrder.None;
+
+            // Initialize the CaseInsensitiveComparer object
+            ObjectCompare = new CaseInsensitiveComparer();
+        }
+
+        /// <summary>
+        /// This method is inherited from the IComparer interface.  It compares the two objects passed using a case insensitive comparison.
+        /// </summary>
+        /// <param name="x">First object to be compared</param>
+        /// <param name="y">Second object to be compared</param>
+        /// <returns>The result of the comparison. "0" if equal, negative if 'x' is less than 'y' and positive if 'x' is greater than 'y'</returns>
+        public int Compare(object x, object y)
+        {
+            int compareResult;
+            ListViewItem listviewX, listviewY;
+
+            // Cast the objects to be compared to ListViewItem objects
+            listviewX = (ListViewItem)x;
+            listviewY = (ListViewItem)y;
+
+            // Compare the two items
+            compareResult = ObjectCompare.Compare(listviewX.SubItems[ColumnToSort].Text, listviewY.SubItems[ColumnToSort].Text);
+
+            // Calculate correct return value based on object comparison
+            if (OrderOfSort == SortOrder.Ascending)
+            {
+                // Ascending sort is selected, return normal result of compare operation
+                return compareResult;
+            }
+            else if (OrderOfSort == SortOrder.Descending)
+            {
+                // Descending sort is selected, return negative result of compare operation
+                return (-compareResult);
+            }
+            else
+            {
+                // Return '0' to indicate they are equal
+                return 0;
+            }
+        }
+
+        /// <summary>
+        /// Gets or sets the number of the column to which to apply the sorting operation (Defaults to '0').
+        /// </summary>
+        public int SortColumn
+        {
+            set
+            {
+                ColumnToSort = value;
+            }
+            get
+            {
+                return ColumnToSort;
+            }
+        }
+
+        /// <summary>
+        /// Gets or sets the order of sorting to apply (for example, 'Ascending' or 'Descending').
+        /// </summary>
+        public SortOrder Order
+        {
+            set
+            {
+                OrderOfSort = value;
+            }
+            get
+            {
+                return OrderOfSort;
+            }
+        }
+
+    }
+	
 
 }
