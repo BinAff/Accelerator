7ea4a57a09cbb71732c74ec600510fa78b740490
ArpanKar <ArpanKar@61b5b83d-0e8f-4795-99eb-9aad62fa1a29>
2013-12-09 11:04:32 +0000

    Adding read sp

--- Project/Crystal/Source Code/Crystal/Guardian/Component/Account/SecurityAnswer/DBScript.sql
+++ Project/Crystal/Source Code/Crystal/Guardian/Component/Account/SecurityAnswer/DBScript.sql
@@ -11,7 +11,22 @@ CREATE TABLE Guardian.SecurityAnswer
 );
 GO
 
-CREATE PROCEDURE [dbo].[SecurityAnswerUpdate]
+CREATE PROCEDURE Guardian.SecurityAnswerRead
+(
+	@Id Numeric(10,0)
+)
+AS
+BEGIN
+	
+	SELECT Id, UserId, QuestionId, Answer 		
+	FROM Guardian.SecurityAnswer
+	WHERE UserId = @Id   
+   
+END
+
+GO
+
+CREATE PROCEDURE Guardian.SecurityAnswerUpdate
 (
 	@UserId Numeric(10,0),
 	@QuestionId Numeric(10,0),
@@ -48,7 +63,7 @@ GO
 SET QUOTED_IDENTIFIER ON
 GO
 
-CREATE PROCEDURE [dbo].[SecurityAnswerDelete]
+CREATE PROCEDURE Guardian.SecurityAnswerDelete
 (
 	@Id Numeric(10,0)
 )
