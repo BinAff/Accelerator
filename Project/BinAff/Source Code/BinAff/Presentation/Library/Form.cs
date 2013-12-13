0461667fcafea3b3ae66a806ef8d3a93b4fd9dcf
ArpanKar <ArpanKar@61b5b83d-0e8f-4795-99eb-9aad62fa1a29>
2013-12-13 09:33:46 +0000

    Adding generic form class for Form category

--- Project/BinAff/Source Code/BinAff/Presentation/Library/Form.cs
+++ Project/BinAff/Source Code/BinAff/Presentation/Library/Form.cs
@@ -1,82 +1,16 @@
 ﻿using System;
 
-using BinAff.Core;
-
 namespace BinAff.Presentation.Library
 {
 
     public partial  class Form : System.Windows.Forms.Form
     {
 
+        public Boolean IsModified { get; protected set; }
+
         public Form()
         {
-            InitializeComponent();
-            
-        }
-
-        public Boolean IsDeleteButton
-        {
-            set
-            {
-                this.btnDelete.Enabled = value;
-            }
-        }
-
-        protected virtual void btnRefresh_Click(object sender, EventArgs e)
-        {
-            this.LoadForm();
-            this.Clear();
-        }
-
-        protected virtual void btnAdd_Click(object sender, EventArgs e)
-        {
-            this.SubmitData();
-        }
-
-        protected virtual void btnDelete_Click(object sender, EventArgs e)
-        {
-            this.DeleteData();
-        }
-
-        protected virtual void btnChange_Click(object sender, EventArgs e)
-        {
-            this.SubmitData();
-        }
-
-        protected virtual void LoadForm()
-        {
-            throw new NotImplementedException();
-        }
-
-        protected virtual void Clear()
-        {
-            throw new NotImplementedException();
-        }
-
-        /// <summary>
-        /// Submit data in dialog box
-        /// </summary>
-        protected virtual void SubmitData()
-        {
-            throw new NotImplementedException();
-        }
-
-        /// <summary>
-        /// Delete data in dialog box
-        /// </summary>
-        protected virtual void DeleteData()
-        {
-            throw new NotImplementedException();
-        }
-
-        protected void ShowMessage(ReturnObject<Boolean> ret)
-        {
-            new MessageBox().Show(ret.MessageList);
-            if (!ret.HasError())
-            {
-                this.LoadForm();
-                this.Clear();
-            }
+            InitializeComponent();            
         }
 
     }
