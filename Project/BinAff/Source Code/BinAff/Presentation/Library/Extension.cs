891e5f2a46f4b3c61248341ea054f94202d19c7e
ArpanKar <ArpanKar@61b5b83d-0e8f-4795-99eb-9aad62fa1a29>
2013-12-24 06:41:21 +0000

    Adding Extension methods for listbox and combobox

---
+++ Project/BinAff/Source Code/BinAff/Presentation/Library/Extension.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections.Generic;
+using System.Windows.Forms;
+
+namespace BinAff.Presentation.Library.Extension
+{
+
+    public static class ListBoxExtender
+    {
+
+        public static void Bind<T>(this ListBox control, List<T> list)
+        {
+            control.Items.Clear();
+            foreach (T t in list)
+            {
+                control.Items.Add(t);
+            }
+        }
+
+        public static List<T> DataSource<T>(this ListBox control)
+        {
+            List<T> dataSource = new List<T>();
+            foreach (T t in control.Items)
+            {
+                dataSource.Add(t);
+            }
+            return dataSource;
+        }
+
+    }
+
+    public static class ComboBoxExtender
+    {
+
+        public static void Bind<T>(this ComboBox control, List<T> list)
+        {
+            control.Items.Clear();
+            foreach (T t in list)
+            {
+                control.Items.Add(t);
+            }
+        }
+
+        public static List<T> DataSource<T>(this ComboBox control)
+        {
+            List<T> dataSource = new List<T>();
+            foreach (T t in control.Items)
+            {
+                dataSource.Add(t);
+            }
+            return dataSource;
+        }
+
+    }
+
+}
