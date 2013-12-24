02d96bdc94e4b6d898a7f8f3c646450fa2d1a1eb
ArpanKar <ArpanKar@61b5b83d-0e8f-4795-99eb-9aad62fa1a29>
2013-12-24 06:05:23 +0000

    Security Handler is to protect misuse of software

---
+++ Project/BinAff/Source Code/BinAff/Tool/SecurityHandler/Module/Data.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace BinAff.Tool.SecurityHandler.Module
+{
+
+    public class Data : BinAff.Core.Data
+    {
+
+        public String Code { get; set; }
+        public String Name { get; set; }
+        public String Description { get; set; }
+        public Boolean IsForm { get; set; }
+        public Boolean IsReport { get; set; }
+        public Boolean IsCatalogue { get; set; }
+
+    }
+
+}
