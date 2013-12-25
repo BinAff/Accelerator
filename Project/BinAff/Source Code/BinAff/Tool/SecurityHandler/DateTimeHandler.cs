c43d7ce7303bd3ba76ba03d6a2d235e969ef6f2a
ArpanKar <ArpanKar@61b5b83d-0e8f-4795-99eb-9aad62fa1a29>
2013-12-25 03:47:08 +0000

    System date change error

---
+++ Project/BinAff/Source Code/BinAff/Tool/SecurityHandler/DateTimeHandler.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Threading;
+
+namespace BinAff.Tool.SecurityHandler
+{
+
+    public class DateTimeHandler
+    {
+
+        public delegate void Changed();
+        public event Changed SystemDateChanged;
+
+        public void Start()
+        {
+            new Thread(() => Check()).Start();
+        }
+
+        private void Check()
+        {
+            DateTime last;
+            DateTime now;
+            while (true)
+            {
+                last = DateTime.Now;
+                Thread.Sleep(5000);
+                now = DateTime.Now;
+                if (Math.Abs(now.Subtract(last).TotalSeconds) > 20)
+                {
+                    this.SystemDateChanged();
+                    break;
+                }
+            }
+        }
+
+    }
+
+}
