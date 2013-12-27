2c5bf421f19bb061c1be8100342436857f0426a9
BirajDhekial <BirajDhekial@61b5b83d-0e8f-4795-99eb-9aad62fa1a29>
2013-12-27 07:56:00 +0000

    Added artifact

---
+++ Project/Crystal/Source Code/Crystal/Lodge/Crystal.Lodge.Component/Room/CheckIn/Navigator/Artifact/Server.cs
@@ -0,0 +1,52 @@
+﻿
+using BinAff.Core;
+using CrystalNavigator = Crystal.Navigator.Component;
+
+namespace Crystal.Lodge.Component.Room.CheckIn.Navigator.Artifact
+{
+    public class Server : CrystalNavigator.Artifact.Server
+    {
+        public Server(Data data)
+            : base(data)
+        {
+
+        }
+
+        protected override void Compose()
+        {
+            this.Name = "Lodge CheckIn Form";
+            this.DataAccess = new Dao((Data)this.Data);
+            this.Validator = new Validator((Data)this.Data);
+        }
+
+        protected override BinAff.Core.Data CreateDataObject()
+        {
+            return new Data
+            {
+                Category = (this.Data as Data).Category
+            };
+        }
+
+        protected override BinAff.Core.Crud CreateInstance(BinAff.Core.Data data)
+        {
+            return new Server(data as Data);
+        }
+
+        protected override BinAff.Core.Crud CreateModuleServerInstance(BinAff.Core.Data moduleData)
+        {
+            //Find out CheckIn data from CheckIn form
+            return new Crystal.Lodge.Component.Room.CheckIn.Server(moduleData as Crystal.Lodge.Component.Room.CheckIn.Data);
+        }
+
+        protected override ReturnObject<bool> DeleteAfter()
+        {
+            if ((this.Data as Data).ModuleData != null && (this.Data as Data).ModuleData.Id > 0)
+            {
+                ICrud crud = new Crystal.Lodge.Component.Room.CheckIn.Server(new Crystal.Lodge.Component.Room.CheckIn.Data() { Id = (this.Data as Data).ModuleData.Id });
+                return crud.Delete();
+            }
+
+            return base.DeleteAfter();
+        }
+    }
+}
---
+++ Project/Crystal/Source Code/Crystal/Lodge/Crystal.Lodge.Component/Room/Reservation/Navigator/Artifact/Dao.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Data;
+
+using CrystalNavigator = Crystal.Navigator.Component;
+
+
+namespace Crystal.Lodge.Component.Room.Reservation.Navigator.Artifact
+{
+    public class Dao : CrystalNavigator.Artifact.Dao
+    {
+        private String DeleteArtifactLinkSPName;
+
+        public Dao(Data data)
+            : base(data)
+        {
+
+        }
+
+        protected override void Compose()
+        {
+            base.Compose();
+            this.DeleteArtifactLinkSPName = "[Lodge].[DeleteReservationFormForArtifact]";
+        }
+
+        protected override bool ReadBefore()
+        {
+            base.CreateConnection();
+            base.CreateCommand("[Lodge].[ReadReservationFormForArtifact]");
+            base.AddInParameter("@ArtifactId", DbType.Int64, this.Data.Id);
+            base.AddInParameter("@Category", DbType.Int64, (this.Data as Data).Category);
+
+            DataSet ds = this.ExecuteDataSet();
+            this.CloseConnection();
+
+            Int64 custId = Convert.IsDBNull(ds.Tables[0].Rows[0]["RoomReservationId"]) ? 0 : Convert.ToInt64(ds.Tables[0].Rows[0]["RoomReservationId"]);
+            if (custId > 0)
+            {
+                (this.Data as Data).ModuleData = new Crystal.Lodge.Component.Room.Reservation.Data
+                {
+                    Id = custId
+                };
+            }
+            return true;
+        }
+
+        protected override BinAff.Core.Data CreateDataObject(Int64 id, Crystal.Navigator.Component.Artifact.Category category)
+        {
+            return new Data
+            {
+                Id = id,
+                Category = category,
+            };
+        }
+
+        protected override Boolean CreateAfterModuleArtifactLink()
+        {
+            Boolean status = true;
+
+            Data artifactData = Data as Data;
+            base.CreateCommand("[Lodge].[InsertReservationFormForArtifact]");
+            if (artifactData.ModuleData.Id == 0)
+            {
+                base.AddInParameter("@ReservationId", DbType.Int64, DBNull.Value);
+            }
+            else
+            {
+                base.AddInParameter("@ReservationId", DbType.Int64, artifactData.ModuleData.Id);
+            }
+            base.AddInParameter("@ArtifactId", DbType.String, artifactData.Id);
+            base.AddInParameter("@Category", DbType.Int64, artifactData.Category);
+            Int32 ret = base.ExecuteNonQuery();
+            if (ret == -2146232060) status = false;//Foreign key violation
+
+            return status;
+        }
+
+        protected override bool DeleteBefore()
+        {
+            return this.DeleteArtifactLink();
+        }
+
+        public bool DeleteArtifactLink()
+        {
+            Boolean status = true;
+            base.CreateCommand(this.DeleteArtifactLinkSPName);
+            base.AddInParameter("@Id", DbType.Int64, this.Data.Id);
+
+            Int32 ret = base.ExecuteNonQuery();
+            if (ret == -2146232060) status = false;//Foreign key violation
+
+            base.CloseConnection();
+
+            return status;
+
+        }
+    }
+}
