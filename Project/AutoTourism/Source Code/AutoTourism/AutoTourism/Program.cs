d55621ed3b62441557a3a1360c41071e95cbb959
ArpanKar <ArpanKar@61b5b83d-0e8f-4795-99eb-9aad62fa1a29>
2013-12-27 06:36:32 +0000

    Date Time manipulation protection done

--- Project/AutoTourism/Source Code/AutoTourism/AutoTourism/Program.cs
+++ Project/AutoTourism/Source Code/AutoTourism/AutoTourism/Program.cs
@@ -2,10 +2,15 @@
 using System.Windows.Forms;
 using System.Configuration;
 
+using BinAff.Tool.SecurityHandler;
+
 namespace AutoTourism
 {
+
     static class Program
     {
+
+        static Form currentForm;
         /// <summary>
         /// The main entry point for the application.
         /// </summary>
@@ -14,13 +19,24 @@ namespace AutoTourism
         {
             try
             {
+                Application.ApplicationExit += Application_ApplicationExit;
+
+                DateTimeHandler dateTimeManipulationHandler = new DateTimeHandler();
+                dateTimeManipulationHandler.SystemDateChanged += handler_SystemDateChanged;
+                dateTimeManipulationHandler.Start();
+
                 Application.EnableVisualStyles();
                 Application.SetCompatibleTextRenderingDefault(false);
                 Type type = Type.GetType(
                     ConfigurationManager.AppSettings["StartUpAssembly"] + "." +
                     ConfigurationManager.AppSettings["StartUpClass"] + ", " +
                     ConfigurationManager.AppSettings["StartUpAssembly"], true);
-                Application.Run((Form)Activator.CreateInstance(type));
+                currentForm = (Form)Activator.CreateInstance(type);
+                Application.Run(currentForm);
+            }
+            catch (System.Data.SqlClient.SqlException ex)
+            {
+                throw ex;
             }
             catch (Exception ex)
             {
@@ -28,5 +44,26 @@ namespace AutoTourism
             }
         }
 
+        static void Application_ApplicationExit(object sender, EventArgs e)
+        {
+            Application.ExitThread();
+        }
+
+        static void handler_SystemDateChanged()
+        {
+            FormCollection formCollection = Application.OpenForms;
+            foreach (Form frm in formCollection)
+            {
+                frm.BeginInvoke(new Action(() => { frm.Enabled = false; }));
+            }
+            new BinAff.Presentation.Library.MessageBox()
+            { 
+                DialogueType = BinAff.Presentation.Library.MessageBox.Type.Error,
+                Heading = "Splash :: Fatal Error!!!",
+            }.Show("System date changed or trying to change.");
+            Application.Exit();
+        }
+
     }
+
 }
