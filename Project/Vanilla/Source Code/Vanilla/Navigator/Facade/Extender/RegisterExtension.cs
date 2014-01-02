663ebddb0b22111fe9005522572344f328970171
BirajDhekial <BirajDhekial@61b5b83d-0e8f-4795-99eb-9aad62fa1a29>
2014-01-02 09:00:38 +0000

    Added moduleBrowser and artifact extension

---
+++ Project/Vanilla/Source Code/Vanilla/Navigator/Facade/Extender/RegisterExtension.cs
@@ -0,0 +1,26 @@
+﻿using System.Windows.Forms;
+
+namespace Vanilla.Navigator.Facade.Extender
+{
+    public static class RegisterExtension
+    {
+        public static TreeNode CreateTreeNodes(this TreeView treeView, Facade.Artifact.Dto node)
+        {
+            TreeNode treeNode = new TreeNode(node.FileName)
+            {
+                Tag = node,
+            };
+            if (node.Children != null && node.Children.Count > 0)
+            {
+                foreach (Facade.Artifact.Dto child in node.Children)
+                {
+                    if (child.Style == Facade.Artifact.Type.Directory)
+                    {
+                        treeNode.Nodes.Add(CreateTreeNodes(treeView, child));
+                    }
+                }
+            }
+            return treeNode;
+        }
+    }
+}
---
+++ Project/Vanilla/Source Code/Vanilla/Navigator/WinForm/ModuleBrowser.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Vanilla.Navigator.WinForm
+{
+    public partial class ModuleBrowser : UserControl
+    {
+        public ModuleBrowser()
+        {
+            InitializeComponent();
+        }
+    }
+}
