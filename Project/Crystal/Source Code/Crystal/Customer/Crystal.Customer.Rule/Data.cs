a538e2f6e5072025ec5bfc69256c62ccb9d51ff5
ArpanKar <ArpanKar@61b5b83d-0e8f-4795-99eb-9aad62fa1a29>
2014-02-04 05:53:09 +0000

    Clean up

--- Project/Crystal/Source Code/Crystal/Customer/Crystal.Customer.Rule/Data.cs
+++ Project/Crystal/Source Code/Crystal/Customer/Crystal.Customer.Rule/Data.cs
@@ -2,11 +2,30 @@
 
 namespace Crystal.Customer.Rule
 {
+
     public class Data : BinAff.Core.Data
     {
+
+        /// <summary>
+        /// Mandatoriness of PIN
+        /// </summary>
         public Boolean IsPinNumber { get; set; }
+
+        /// <summary>
+        /// Minimum two contact number
+        /// </summary>
         public Boolean IsAlternateContactNumber { get; set; }
+
+        /// <summary>
+        /// Mandatoriness of Email
+        /// </summary>
         public Boolean IsEmail { get; set; }
+
+        /// <summary>
+        /// Mandatoriness of identity proof
+        /// </summary>
         public Boolean IsIdentityProof { get; set; }
+
     }
+
 }
--- Project/Crystal/Source Code/Crystal/Customer/Crystal.Customer.Rule/Validator.cs
+++ Project/Crystal/Source Code/Crystal/Customer/Crystal.Customer.Rule/Validator.cs
@@ -2,9 +2,11 @@
 using BinAff.Core;
 
 namespace Crystal.Customer.Rule
+
 {
     public class Validator : BinAff.Core.Validator
     {
+
         public Validator(Data data)
             : base(data)
         {
@@ -13,8 +15,9 @@ namespace Crystal.Customer.Rule
 
         protected override List<Message> Validate()
         {
-            List<Message> retMsg = new List<Message>();
-            return retMsg;
+            return new List<Message>();
         }
+
     }
+
 }
