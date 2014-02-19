94c9d7641f2fb2c8ab4043e07b0bc2ee276f7346
ArpanKar <ArpanKar@61b5b83d-0e8f-4795-99eb-9aad62fa1a29>
2014-02-19 04:29:49 +0000

    Introducing Form

---
+++ Project/Vanilla/Source Code/Vanilla/Form/Vanilla.Form.WinForm/Form.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace BinAff.Presentation.Library
+{
+
+    public partial  class Form : System.Windows.Forms.Form
+    {
+
+        public Boolean IsModified { get; protected set; }
+
+        public Form()
+        {
+            InitializeComponent();            
+        }
+
+    }
+
+}
