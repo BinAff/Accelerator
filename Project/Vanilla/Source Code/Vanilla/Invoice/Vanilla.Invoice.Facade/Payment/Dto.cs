84381b0a45d4552959e35a8d7e5064e48e094377
BirajDhekial <BirajDhekial@61b5b83d-0e8f-4795-99eb-9aad62fa1a29>
2014-03-17 06:33:32 +0000

    code for invoice

---
+++ Project/Vanilla/Source Code/Vanilla/Invoice/Vanilla.Invoice.Facade/Payment/Dto.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Vanilla.Invoice.Facade.Payment
+{
+    public class Dto
+    {
+        public DateTime date { get; set; }
+        public String cardNumber { get; set; }
+        public String remark { get; set; }
+        public Double amount {get;set;}
+        public String paymentType { get; set; }
+        //public Type.Dto Type { get; set; }
+    }
+    
+}
--- Project/Vanilla/Source Code/Vanilla/Invoice/Vanilla.Invoice.WinForm/Taxation.Designer.cs
+++ Project/Vanilla/Source Code/Vanilla/Invoice/Vanilla.Invoice.WinForm/Taxation.Designer.cs
@@ -28,11 +28,114 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.lslList = new System.Windows.Forms.ListBox();
+            this.lblTaxName = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.chkIsPerc = new System.Windows.Forms.CheckBox();
+            this.lblTaxVal = new System.Windows.Forms.Label();
+            this.txtTaxValue = new System.Windows.Forms.TextBox();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.btnChange = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lslList
+            // 
+            this.lslList.FormattingEnabled = true;
+            this.lslList.Location = new System.Drawing.Point(10, 12);
+            this.lslList.Name = "lslList";
+            this.lslList.Size = new System.Drawing.Size(172, 108);
+            this.lslList.Sorted = true;
+            this.lslList.TabIndex = 153;
+            // 
+            // lblTaxName
+            // 
+            this.lblTaxName.AutoSize = true;
+            this.lblTaxName.Location = new System.Drawing.Point(200, 15);
+            this.lblTaxName.Name = "lblTaxName";
+            this.lblTaxName.Size = new System.Drawing.Size(56, 13);
+            this.lblTaxName.TabIndex = 154;
+            this.lblTaxName.Text = "Tax Name";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(262, 12);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(164, 20);
+            this.textBox1.TabIndex = 155;
+            // 
+            // chkIsPerc
+            // 
+            this.chkIsPerc.AutoSize = true;
+            this.chkIsPerc.Location = new System.Drawing.Point(263, 68);
+            this.chkIsPerc.Name = "chkIsPerc";
+            this.chkIsPerc.Size = new System.Drawing.Size(92, 17);
+            this.chkIsPerc.TabIndex = 156;
+            this.chkIsPerc.Text = "Is Percentage";
+            this.chkIsPerc.UseVisualStyleBackColor = true;
+            // 
+            // lblTaxVal
+            // 
+            this.lblTaxVal.AutoSize = true;
+            this.lblTaxVal.Location = new System.Drawing.Point(201, 49);
+            this.lblTaxVal.Name = "lblTaxVal";
+            this.lblTaxVal.Size = new System.Drawing.Size(55, 13);
+            this.lblTaxVal.TabIndex = 157;
+            this.lblTaxVal.Text = "Tax Value";
+            // 
+            // txtTaxValue
+            // 
+            this.txtTaxValue.Location = new System.Drawing.Point(262, 42);
+            this.txtTaxValue.Name = "txtTaxValue";
+            this.txtTaxValue.Size = new System.Drawing.Size(93, 20);
+            this.txtTaxValue.TabIndex = 158;
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Location = new System.Drawing.Point(436, 10);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(75, 23);
+            this.btnAdd.TabIndex = 159;
+            this.btnAdd.Text = "Add";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            // 
+            // btnChange
+            // 
+            this.btnChange.Location = new System.Drawing.Point(436, 39);
+            this.btnChange.Name = "btnChange";
+            this.btnChange.Size = new System.Drawing.Size(75, 23);
+            this.btnChange.TabIndex = 160;
+            this.btnChange.Text = "Change";
+            this.btnChange.UseVisualStyleBackColor = true;
+            // 
+            // Taxation
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(518, 130);
+            this.Controls.Add(this.btnChange);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.txtTaxValue);
+            this.Controls.Add(this.lblTaxVal);
+            this.Controls.Add(this.chkIsPerc);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.lblTaxName);
+            this.Controls.Add(this.lslList);
+            this.Name = "Taxation";
             this.Text = "Taxation";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.ListBox lslList;
+        private System.Windows.Forms.Label lblTaxName;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.CheckBox chkIsPerc;
+        private System.Windows.Forms.Label lblTaxVal;
+        private System.Windows.Forms.TextBox txtTaxValue;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.Button btnChange;
     }
 }
