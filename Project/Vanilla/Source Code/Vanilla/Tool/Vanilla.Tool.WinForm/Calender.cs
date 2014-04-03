528de75f7478d8da75a3e5cd2effb698fe75d92d
ArpanKar <ArpanKar@61b5b83d-0e8f-4795-99eb-9aad62fa1a29>
2014-04-03 04:28:24 +0000

    WIP - Diary

---
+++ Project/Vanilla/Source Code/Vanilla/Tool/Vanilla.Tool.WinForm/Calender.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace Vanilla.Tool.WinForm
+{
+    public partial class Calender : Form
+    {
+
+        Facade.Diary.Appointment.FormDto formDto;
+
+        public Calender()
+        {
+            InitializeComponent();
+        }
+
+        private void Calender_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void dtpDate_ValueChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void btnGet_Click(object sender, EventArgs e)
+        {
+            this.dgvAppointmentList.DataSource = new Facade.Diary.Calender.Server(null).Search(this.dtpDate.Value).ConvertAll(c => new
+            {
+                c.Start,
+                c.Description
+            });
+        }
+
+        private void dgvAppointmentList_CellFormatting(object sender, DataGridViewCellFormattingEventArgs e)
+        {
+            if (e.ColumnIndex == 1 && !String.IsNullOrEmpty(Convert.ToString(e.Value)) )
+            {
+                e.CellStyle.BackColor = System.Drawing.Color.Gray;
+            }
+        }
+
+    }
+
+}
