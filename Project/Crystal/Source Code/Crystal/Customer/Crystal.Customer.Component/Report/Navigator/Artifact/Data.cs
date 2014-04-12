d7d6ebcac7d8770db94448cc111f0d94965a11cb
BirajDhekial <BirajDhekial@61b5b83d-0e8f-4795-99eb-9aad62fa1a29>
2014-04-12 10:51:38 +0000

    added customer report component

---
+++ Project/Crystal/Source Code/Crystal/Customer/Crystal.Customer.Component/Report/Navigator/Artifact/Data.cs
@@ -0,0 +1,8 @@
+﻿using CrystalNavigator = Crystal.Navigator.Component;
+
+namespace Crystal.Customer.Component.Report.Navigator.Artifact
+{
+    public class Data : CrystalNavigator.Artifact.Data
+    {
+    }
+}
