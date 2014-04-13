3a9adf19c3bbbdf6fe091b7cc706ee65735ca8f2
BirajDhekial <BirajDhekial@61b5b83d-0e8f-4795-99eb-9aad62fa1a29>
2014-04-13 06:10:55 +0000

    added interface for Report

---
+++ Project/Vanilla/Source Code/Vanilla/Utility/Vanilla.Utility.Facade/Report/Category/IReport.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Vanilla.Utility.Facade.Report.Category
+{
+    public interface IReport
+    {
+        List<Facade.Report.Dto> GetDailyReport(DateTime date);
+        List<Facade.Report.Dto> GetWeeklyReport(DateTime date);
+        List<Facade.Report.Dto> GetMonthlyReport(DateTime date);
+        List<Facade.Report.Dto> GetQuarterlyReport(DateTime date);
+        List<Facade.Report.Dto> GetYearlyReport(DateTime date);
+    }
+}
