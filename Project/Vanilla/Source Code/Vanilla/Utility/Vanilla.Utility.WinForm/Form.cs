fa33b982adc4301c130f5e43a172acf54384393f
BirajDhekial <BirajDhekial@61b5b83d-0e8f-4795-99eb-9aad62fa1a29>
2014-04-30 23:15:57 +0000

    added Form

---
+++ Project/Vanilla/Source Code/Vanilla/Utility/Vanilla.Utility.WinForm/Form.cs
@@ -0,0 +1,16 @@
+﻿
+using PresentationLibrary = BinAff.Presentation.Library;
+using FacadeArtifact = Vanilla.Utility.Facade.Artifact;
+
+namespace Vanilla.Utility.WinForm
+{
+    public partial class Form : PresentationLibrary.Form
+    {
+        public FacadeArtifact.Dto ArtifactDto { get; protected set; }
+
+        public Form()
+        {
+            InitializeComponent();
+        }
+    }
+}
