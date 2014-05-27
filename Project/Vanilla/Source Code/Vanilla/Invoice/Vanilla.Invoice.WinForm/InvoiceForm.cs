428cf96248e8542201bedcfcb412a388e084692c
ArpanKar <ArpanKar@61b5b83d-0e8f-4795-99eb-9aad62fa1a29>
2014-05-27 15:56:06 +0000

    It should come from Document base class

--- Project/Vanilla/Source Code/Vanilla/Invoice/Vanilla.Invoice.WinForm/InvoiceForm.cs
+++ Project/Vanilla/Source Code/Vanilla/Invoice/Vanilla.Invoice.WinForm/InvoiceForm.cs
@@ -6,15 +6,25 @@ using System.Linq;
 using System.Text;
 using System.Windows.Forms;
 
+using FormWin = Vanilla.Form.WinForm;
+using UtilFac = Vanilla.Utility.Facade;
+using DocFac = Vanilla.Utility.Facade.Document;
+
 namespace Vanilla.Invoice.WinForm
 {
 
-    public partial class InvoiceForm : System.Windows.Forms.Form
+    public partial class InvoiceForm : FormWin.Document
     {
 
         public InvoiceForm()
         {
             InitializeComponent();
+        }
+
+        public InvoiceForm(UtilFac.Artifact.Dto artifact)
+            : base(artifact)
+        {
+            InitializeComponent();            
         }
 
         private void btnGenerate_Click(object sender, EventArgs e)
@@ -32,6 +42,43 @@ namespace Vanilla.Invoice.WinForm
         {
             //Can be added later
             //Disabled initially, enabled after generation
+        }
+
+        protected override void Compose()
+        {
+            base.formDto = new Facade.FormDto
+            {
+                ModuleFormDto = new UtilFac.Module.FormDto(),
+                Dto = new Facade.Dto()
+            };
+        }
+
+        protected override DocFac.Dto CloneDto(DocFac.Dto source)
+        {
+            return new DocFac.Dto();
+        }
+
+        protected override void LoadForm()
+        {
+            //BinAff.Facade.Library.Server facade = new Facade.Server(formDto);
+            //facade.LoadForm();
+
+            //Facade.FormDto formDto = base.formDto as Facade.FormDto;
+            //base.facade.LoadForm();
+        }
+
+        protected override void PopulateDataToForm()
+        {
+
+        }
+
+        protected override Boolean ValidateForm()
+        {
+            return true;
+        }
+
+        protected override void AssignDto()
+        {
         }
 
     }
