7769e1620d47f71d3c1f76dd12308f243b6f29e1
BirajDhekial <BirajDhekial@61b5b83d-0e8f-4795-99eb-9aad62fa1a29>
2014-05-30 16:19:53 +0000

    Added Receipt files

---
+++ Project/Vanilla/Source Code/Vanilla/Invoice/Vanilla.Invoice.Facade/Receipt/FormDto.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Vanilla.Invoice.Facade.Receipt
+{
+    public class FormDto
+    {
+        public Vanilla.Utility.Facade.Artifact.Dto ArtifactDto { get; set; }
+    }
+}
