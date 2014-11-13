72ff7e6ec6613ce3c988b877848a30917ec9a498
ArpanKar <ArpanKar@61b5b83d-0e8f-4795-99eb-9aad62fa1a29>
2014-11-13 11:30:20 +0000

    ReadAll implemented in Room, Room Type and Room Category. Resolved side effects for this implementations

--- Project/BinAff/Source Code/BinAff/Facade/Library/Server.cs
+++ Project/BinAff/Source Code/BinAff/Facade/Library/Server.cs
@@ -65,13 +65,19 @@ namespace BinAff.Facade.Library
 
         public virtual List<T> ReadAll<T>() where T : Dto
         {
-            List<Dto> ret = this.ReadAllInternal();
+            ReturnObject<List<BinAff.Core.Data>> categoryList = this.AssignComponentServer(null).ReadAll();
+            this.IsError = categoryList.HasError();
+            this.DisplayMessageList = categoryList.GetMessage(this.IsError ? Message.Type.Error : Message.Type.Information);
+            List<Dto> ret = null;
+            if (!this.IsError)
+            {
+                ret = new List<Dto>();
+                foreach (BinAff.Core.Data data in categoryList.Value)
+                {
+                    ret.Add(this.Convert(data));
+                }
+            }
             return ret.ConvertAll<T>((p) => { return (T)p; });
-        }
-
-        protected virtual List<Dto> ReadAllInternal()
-        {
-            throw new NotImplementedException();
         }
 
         /// <summary>
@@ -83,6 +89,11 @@ namespace BinAff.Facade.Library
         /// Convert business data from DTO
         /// </summary>
         public abstract Core.Data Convert(Dto dto);
+
+        protected virtual ICrud AssignComponentServer(Data data)
+        {
+            throw new NotImplementedException();
+        }
  
     }
 
