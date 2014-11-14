f8d21696075971bd14883a0c9868b96a7f03b18d
ArpanKar <ArpanKar@61b5b83d-0e8f-4795-99eb-9aad62fa1a29>
2014-11-14 11:21:35 +0000

    Changing AutoTourism project to start up project and implementing Room list inside cache

---
+++ Project/AutoTourism/Source Code/AutoTourism/Utility/AutoTourism.Utility.Facade/Cache/Dto.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+
+namespace AutoTourism.Utility.Facade.Cache
+{
+
+    public class Dto : Vanilla.Utility.Facade.Cache.Dto
+    {
+
+        public List<Lodge.Configuration.Facade.Room.Dto> RoomList;
+
+    }
+
+}
---
+++ Project/AutoTourism/Source Code/AutoTourism/Utility/AutoTourism.Utility.Facade/Cache/Server.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+
+using RoomFac = AutoTourism.Lodge.Configuration.Facade.Room;
+
+namespace AutoTourism.Utility.Facade.Cache
+{
+
+    public class Server
+    {
+
+        public Boolean Cache()
+        {
+            BinAff.Facade.Cache.Server.Current.Cache["Autotourism"] = new Dto
+            {
+                RoomList = new RoomFac.Server(null).ReadAll<RoomFac.Dto>(),
+            };
+            return true;
+        }
+
+        public Boolean Refresh()
+        {
+            Dto cache = BinAff.Facade.Cache.Server.Current.Cache["Autotourism"] as Dto;
+            cache.RoomList = new RoomFac.Server(null).ReadAll<RoomFac.Dto>();
+            return true;
+        }
+
+    }
+
+}
