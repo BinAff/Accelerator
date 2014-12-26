d31224990919fa50bffd2ed36d58b9bab0c3c966
ArpanKar <ArpanKar@61b5b83d-0e8f-4795-99eb-9aad62fa1a29>
2014-12-26 08:06:56 +0000

    CreateObject to row implemented

--- Project/Crystal/Source Code/Crystal/Product/Crystal.Product.Component/Dao.cs
+++ Project/Crystal/Source Code/Crystal/Product/Crystal.Product.Component/Dao.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Data;
 
 namespace Crystal.Product.Component
@@ -14,8 +13,6 @@ namespace Crystal.Product.Component
 
         }
 
-        protected abstract override void Compose();
-
         protected override void AssignParameter(string procedureName)
         {
             base.AddInParameter("@Number", DbType.String, ((Data)this.Data).Number);
@@ -23,43 +20,14 @@ namespace Crystal.Product.Component
             base.AddInParameter("@Description", DbType.String, ((Data)this.Data).Description);
         }
 
-        protected override BinAff.Core.Data CreateDataObject(DataSet ds, BinAff.Core.Data data)
+        protected override BinAff.Core.Data CreateDataObject(DataRow dr, BinAff.Core.Data data)
         {
-            Data dt = (Data)data;
-            DataRow row;
-            if (ds != null && ds.Tables.Count > 0 && ds.Tables[0].Rows.Count > 0)
-            {
-                row = ds.Tables[0].Rows[0];
-
-                dt.Number = Convert.IsDBNull(row["Number"]) ? String.Empty : Convert.ToString(row["Number"]);
-                dt.Name = Convert.IsDBNull(row["Name"]) ? String.Empty : Convert.ToString(row["Name"]);
-                dt.Description = Convert.IsDBNull(row["Description"]) ? String.Empty : Convert.ToString(row["Description"]);
-            }
+            Data dt = data as Data;
+            dt.Number = Convert.IsDBNull(dr["Number"]) ? String.Empty : Convert.ToString(dr["Number"]);
+            dt.Name = Convert.IsDBNull(dr["Name"]) ? String.Empty : Convert.ToString(dr["Name"]);
+            dt.Description = Convert.IsDBNull(dr["Description"]) ? String.Empty : Convert.ToString(dr["Description"]);
             return dt;
         }
-
-        protected override List<BinAff.Core.Data> CreateDataObjectList(DataSet ds)
-        {
-            List<BinAff.Core.Data> ret = new List<BinAff.Core.Data>();
-
-            //if (ds != null && ds.Tables.Count > 0 && ds.Tables[0].Rows.Count > 0)
-            //{
-            //    foreach (DataRow row in ds.Tables[0].Rows)
-            //    {
-            //        //Data data = this.CreateDataObject();
-            //        //data.Id = Convert.IsDBNull(row["Id"]) ? 0 : Convert.ToInt64(row["Id"]);
-            //        //data.Product = this.BindItem(Convert.IsDBNull(row["ItemId"]) ? 0 : Convert.ToInt32(row["ItemId"]));
-            //        //data.StartDate = Convert.IsDBNull(row["StartDate"]) ? DateTime.MinValue : Convert.ToDateTime(row["StartDate"]);
-            //        //data.EndDate = Convert.IsDBNull(row["EndDate"]) ? DateTime.MinValue : Convert.ToDateTime(row["EndDate"]);
-            //        //data.Rate = Convert.IsDBNull(row["Rate"]) ? 0 : Convert.ToDouble(row["Rate"]);
-            //        ret.Add(data);
-            //    }
-            //}
-            return ret;
-        }
-
-        //protected abstract Product.Component.Data BindItem(Int64 itemId);
-        //protected abstract Data CreateDataObject();
 
     }
 
