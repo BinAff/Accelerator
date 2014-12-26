8f72f37bc38477b5b0e3675a32e4c92f27689abf
ArpanKar <ArpanKar@61b5b83d-0e8f-4795-99eb-9aad62fa1a29>
2014-12-26 08:16:07 +0000

    Introducing Accomodation and Extra Accomodation

--- Project/AutoTourism/Source Code/AutoTourism/Lodge/Configuration/AutoTourism.Lodge.Configuration.Facade/Room/Dto.cs
+++ Project/AutoTourism/Source Code/AutoTourism/Lodge/Configuration/AutoTourism.Lodge.Configuration.Facade/Room/Dto.cs
@@ -10,14 +10,35 @@ namespace AutoTourism.Lodge.Configuration.Facade.Room
     {
 
         public String Number { get; set; }
+
         public String Name { get; set; }
+
+        public String Display
+        {
+            get
+            {
+                return this.Building.Name +  ":" + this.Number + "-" + this.Name;
+            }
+        }
+
         public String Description { get; set; }
+
         public Room.Category.Dto Category { get; set; }
+
         public Room.Type.Dto Type { get; set; }
+
         public Building.Dto Building { get; set; }
-        public Table Floor { get; set; }                
-        public Boolean IsAirconditioned { get; set; }       
+
+        public Table Floor { get; set; }
+   
+        public Boolean IsAirconditioned { get; set; }
+
+        public Int16 Accomodation { get; set; }
+
+        public Int16 ExtraAccomodation { get; set; }
+       
         public Int64 StatusId { get; set; }
+
         public List<Image.Dto> ImageList { get; set; }
         //public Boolean IsDormitory { get; set; }
         
