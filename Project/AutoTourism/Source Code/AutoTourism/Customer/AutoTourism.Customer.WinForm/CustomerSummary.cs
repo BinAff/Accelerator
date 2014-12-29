5d0cb7683a15f06f65873380236332c4764ed88f
ArpanKar <ArpanKar@61b5b83d-0e8f-4795-99eb-9aad62fa1a29>
2014-12-29 04:55:24 +0000

    Adding customer summary user control for reusability

---
+++ Project/AutoTourism/Source Code/AutoTourism/Customer/AutoTourism.Customer.WinForm/CustomerSummary.cs
@@ -0,0 +1,31 @@
+﻿using System.Windows.Forms;
+
+using BinAff.Presentation.Library.Extension;
+
+namespace AutoTourism.Customer.WinForm
+{
+
+    public partial class CustomerSummary : UserControl
+    {
+
+        public CustomerSummary()
+        {
+            InitializeComponent();
+        }
+
+        public void Load(Facade.Dto dto)
+        {
+            Facade.Dto data = dto as Facade.Dto;
+            if (data != null)
+            {
+                this.txtName.Text = data.Name;
+
+                this.lstContact.Bind(data.ContactNumberList, "Name");
+                this.txtAdds.Text = data.Address;
+                this.txtEmail.Text = data.Email;
+            }
+        }
+
+    }
+
+}
