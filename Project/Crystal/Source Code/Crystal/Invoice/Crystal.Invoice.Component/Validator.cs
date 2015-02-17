6a2c891c45a3b93a23f548fba681615e33f25304
ArpanKar <ArpanKar@61b5b83d-0e8f-4795-99eb-9aad62fa1a29>
2015-02-17 06:06:02 +0000

    Bug fix: Null check for Seller and Buyer

--- Project/Crystal/Source Code/Crystal/Invoice/Crystal.Invoice.Component/Validator.cs
+++ Project/Crystal/Source Code/Crystal/Invoice/Crystal.Invoice.Component/Validator.cs
@@ -38,61 +38,75 @@ namespace Crystal.Invoice.Component
             {
                 retMsg.Add(new Message("Discount cannot be less than 0.", Message.Type.Error));
             }
-            if (ValidationRule.IsNullOrEmpty(data.Seller.Name))
+            if (data.Seller == null)
             {
-                retMsg.Add(new Message("Seller name cannot be empty.", Message.Type.Error));
+                retMsg.Add(new Message("Seller is mandatory.", Message.Type.Error));
             }
-            else if (data.Seller.Name.Length > 50)
+            else
             {
-                retMsg.Add(new Message("Seller name length cannot be greater than 50.", Message.Type.Error));
+                if (ValidationRule.IsNullOrEmpty(data.Seller.Name))
+                {
+                    retMsg.Add(new Message("Seller name cannot be empty.", Message.Type.Error));
+                }
+                else if (data.Seller.Name.Length > 50)
+                {
+                    retMsg.Add(new Message("Seller name length cannot be greater than 50.", Message.Type.Error));
+                }
+                if (ValidationRule.IsNullOrEmpty(data.Seller.Address))
+                {
+                    retMsg.Add(new Message("Seller address cannot be empty.", Message.Type.Error));
+                }
+                else if (data.Seller.Address.Length > 250)
+                {
+                    retMsg.Add(new Message("Seller address length cannot be greater than 250.", Message.Type.Error));
+                }
+                if (ValidationRule.IsNullOrEmpty(data.Seller.Liscence))
+                {
+                    retMsg.Add(new Message("Seller liscence cannot be empty.", Message.Type.Error));
+                }
+                else if (data.Seller.Liscence.Length > 50)
+                {
+                    retMsg.Add(new Message("Seller liscence length cannot be greater than 50.", Message.Type.Error));
+                }
+                if (ValidationRule.IsNullOrEmpty(data.Seller.ContactNumber))
+                {
+                    retMsg.Add(new Message("Seller contact Number cannot be empty.", Message.Type.Error));
+                }
+                else if (data.Seller.ContactNumber.Length > 50)
+                {
+                    retMsg.Add(new Message("Seller contact Number length cannot be greater than 50.", Message.Type.Error));
+                }
+                if (!ValidationRule.IsNullOrEmpty(data.Seller.Email) && !ValidationRule.IsEmailId(data.Seller.Email))
+                {
+                    retMsg.Add(new Message("Seller Email is not valid.", Message.Type.Error));
+                }
             }
-            if (ValidationRule.IsNullOrEmpty(data.Seller.Address))
+            if (data.Buyer == null)
             {
-                retMsg.Add(new Message("Seller address cannot be empty.", Message.Type.Error));
+                retMsg.Add(new Message("Buyer is mandatory.", Message.Type.Error));
             }
-            else if (data.Seller.Address.Length > 250)
+            else
             {
-                retMsg.Add(new Message("Seller address length cannot be greater than 250.", Message.Type.Error));
-            }
-            if (ValidationRule.IsNullOrEmpty(data.Seller.Liscence))
-            {
-                retMsg.Add(new Message("Seller liscence cannot be empty.", Message.Type.Error));
-            }
-            else if (data.Seller.Liscence.Length > 50)
-            {
-                retMsg.Add(new Message("Seller liscence length cannot be greater than 50.", Message.Type.Error));
-            }
-            if (ValidationRule.IsNullOrEmpty(data.Seller.ContactNumber))
-            {
-                retMsg.Add(new Message("Seller contact Number cannot be empty.", Message.Type.Error));
-            }
-            else if (data.Seller.ContactNumber.Length > 50)
-            {
-                retMsg.Add(new Message("Seller contact Number length cannot be greater than 50.", Message.Type.Error));
-            }
-            if (!ValidationRule.IsNullOrEmpty(data.Seller.Email) && !ValidationRule.IsEmailId(data.Seller.Email))
-            {
-                retMsg.Add(new Message("Seller Email is not valid.", Message.Type.Error));
-            }
-            if (ValidationRule.IsNullOrEmpty(data.Buyer.Name))
-            {
-                retMsg.Add(new Message("Buyer name cannot be empty.", Message.Type.Error));
-            }
-            else if (data.Buyer.Name.Length > 50)
-            {
-                retMsg.Add(new Message("Buyer name length cannot be greater than 50.", Message.Type.Error));
-            }
-            if (!ValidationRule.IsNullOrEmpty(data.Buyer.Address) && data.Buyer.Address.Length > 250)
-            {
-                retMsg.Add(new Message("Buyer address length cannot be greater than 250.", Message.Type.Error));
-            }
-            if (!ValidationRule.IsNullOrEmpty(data.Buyer.ContactNumber) && data.Buyer.ContactNumber.Length > 50)
-            {
-                retMsg.Add(new Message("Buyer contact number length cannot be greater than 50.", Message.Type.Error));
-            }
-            if (!ValidationRule.IsNullOrEmpty(data.Buyer.Email) && !ValidationRule.IsEmailId(data.Buyer.Email))
-            {
-                retMsg.Add(new Message("Buyer Email is not valid.", Message.Type.Error));
+                if (ValidationRule.IsNullOrEmpty(data.Buyer.Name))
+                {
+                    retMsg.Add(new Message("Buyer name cannot be empty.", Message.Type.Error));
+                }
+                else if (data.Buyer.Name.Length > 50)
+                {
+                    retMsg.Add(new Message("Buyer name length cannot be greater than 50.", Message.Type.Error));
+                }
+                if (!ValidationRule.IsNullOrEmpty(data.Buyer.Address) && data.Buyer.Address.Length > 250)
+                {
+                    retMsg.Add(new Message("Buyer address length cannot be greater than 250.", Message.Type.Error));
+                }
+                if (!ValidationRule.IsNullOrEmpty(data.Buyer.ContactNumber) && data.Buyer.ContactNumber.Length > 50)
+                {
+                    retMsg.Add(new Message("Buyer contact number length cannot be greater than 50.", Message.Type.Error));
+                }
+                if (!ValidationRule.IsNullOrEmpty(data.Buyer.Email) && !ValidationRule.IsEmailId(data.Buyer.Email))
+                {
+                    retMsg.Add(new Message("Buyer Email is not valid.", Message.Type.Error));
+                }
             }
             if (data.LineItem == null || data.LineItem.Count <= 0)
             {
