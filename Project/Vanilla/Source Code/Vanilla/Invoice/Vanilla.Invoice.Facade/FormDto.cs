0aee4ab7f0f562e059c22a98728a7faf407212bd
ArpanKar <ArpanKar@61b5b83d-0e8f-4795-99eb-9aad62fa1a29>
2015-04-30 08:45:11 +0000

    Cleanup

--- Project/Vanilla/Source Code/Vanilla/Invoice/Vanilla.Invoice.Facade/FormDto.cs
+++ Project/Vanilla/Source Code/Vanilla/Invoice/Vanilla.Invoice.Facade/FormDto.cs
@@ -3,13 +3,16 @@ using System.Collections.Generic;
 
 namespace Vanilla.Invoice.Facade
 {
+
     public class FormDto : Vanilla.Form.Facade.Document.FormDto
     {
-        //public Dto dto { get; set; }
+
         public List<Table> paymentTypeList { get; set; }
 
         public Vanilla.Utility.Facade.Module.FormDto ModuleFormDto { get; set; }
+
     }
+
 }
 
 
