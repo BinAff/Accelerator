827cde19380050e790e96094f1ae8ac19b8ea5ca
Arpan Kar <arpan.kar@binaryaffairs.com>
2015-05-09 16:19:19 +0530

    Cleanup

--- Project/Crystal/Source Code/Crystal/Report/Crystal.Report.Component/Dao.cs
+++ Project/Crystal/Source Code/Crystal/Report/Crystal.Report.Component/Dao.cs
@@ -18,29 +18,17 @@ namespace Crystal.Report.Component
 
         protected override void AssignParameter(string procedureName)
         {
-            base.AddInParameter("@Date", DbType.DateTime, ((Data)this.Data).Date);
-            base.AddInParameter("@CategoryId", DbType.Int64, ((Data)this.Data).Category.Id);
+            base.AddInParameter("@Date", DbType.DateTime, (this.Data as Data).Date);
+            base.AddInParameter("@CategoryId", DbType.Int64, (this.Data as Data).Category.Id);
         }
 
-        protected override BinAff.Core.Data CreateDataObject(DataSet ds, BinAff.Core.Data data)
+        protected override BinAff.Core.Data CreateDataObject(DataRow dr, BinAff.Core.Data data)
         {
-            Data dt = (Data)data;
-            DataRow row;
-            if (ds != null && ds.Tables.Count > 0 && ds.Tables[0].Rows.Count > 0)
-            {
-                row = ds.Tables[0].Rows[0];
-
-                dt.Id = data.Id;
-                dt.Date = Convert.IsDBNull(row["Date"]) ? DateTime.MinValue : Convert.ToDateTime(row["Date"]);
-                dt.Category = Convert.IsDBNull(row["ReportCategoryId"]) ? null : new Category.Data { Id = Convert.ToInt64(row["ReportCategoryId"]) };                
-            }
+            Data dt = data as Data;
+            dt.Id = data.Id;
+            dt.Date = Convert.IsDBNull(dr["Date"]) ? DateTime.MinValue : Convert.ToDateTime(dr["Date"]);
+            dt.Category = Convert.IsDBNull(dr["ReportCategoryId"]) ? null : new Category.Data { Id = Convert.ToInt64(dr["ReportCategoryId"]) };
             return dt;
-        }
-
-        protected override List<BinAff.Core.Data> CreateDataObjectList(DataSet ds)
-        {
-            List<BinAff.Core.Data> ret = new List<BinAff.Core.Data>();
-            return ret;
         }
 
         public abstract List<BinAff.Core.Data> GetData(DateTime fromDate, DateTime toDate);
