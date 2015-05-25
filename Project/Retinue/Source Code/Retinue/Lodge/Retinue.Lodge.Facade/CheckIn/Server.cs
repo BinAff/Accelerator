bc3626f07cd7b1efcf0a748228869ec2a06187a2
Arpan Kar <arpan.kar@binaryaffairs.com>
2015-05-25 09:40:48 +0530

    Removed else condition... checking no of days will move to WinForm

--- Project/Retinue/Source Code/Retinue/Lodge/Retinue.Lodge.Facade/CheckIn/Server.cs
+++ Project/Retinue/Source Code/Retinue/Lodge/Retinue.Lodge.Facade/CheckIn/Server.cs
@@ -182,74 +182,29 @@ namespace Retinue.Lodge.Facade.CheckIn
         public void CheckOut()
         {
             Dto dto = (this.FormDto as FormDto).Dto as Facade.CheckIn.Dto;
+
+            //To Do :: This code will be moved to WinForm
             Int32 noOfDays = new Calender().DaysBetweenTwoDays(dto.Reservation.BookingFrom, DateTime.Today);
-           // if (noOfDays != dto.Reservation.NoOfDays)
-            if (noOfDays > dto.Reservation.NoOfDays)
+            if (noOfDays != dto.Reservation.NoOfDays)
             {
                 this.DisplayMessageList = new List<String>
                 {
                     "Check out date is not matching with reservation end date. Reservation end date will be changed with checkout.",
                 };
             }
-            else
-            {
-                dto.Reservation.NoOfDays = noOfDays == 0 ? 1 : noOfDays;
-                dto.Reservation.Status = RoomRsvFac.Status.CheckOut;
-                dto.Reservation.IsBackDateEntry = true;
 
-                RoomChkCrys.Data checkinData = this.Convert(dto) as RoomChkCrys.Data;
-                ReturnObject<Boolean> ret = (new RoomChkCrys.Server(checkinData) as RoomChkCrys.ICheckIn).CheckOut();
+            dto.Reservation.NoOfDays = noOfDays == 0 ? 1 : noOfDays;
+            dto.Reservation.Status = RoomRsvFac.Status.CheckOut;
+            dto.Reservation.IsBackDateEntry = true;
+            RoomChkCrys.Data checkinData = this.Convert(dto) as RoomChkCrys.Data;
+            ReturnObject<Boolean> ret = (new RoomChkCrys.Server(checkinData) as RoomChkCrys.ICheckIn).CheckOut();
+
+            this.DisplayMessageList = ret.MessageList.ConvertAll((p) => { return p.Description; });
+            if (!(this.IsError = !ret.Value))            
+            {
                 dto.Status = RoomRsvFac.Status.CheckOut;
                 dto.CheckOutTime = checkinData.CompletionTime;
-                this.DisplayMessageList = (this.IsError = !ret.Value) ?
-                    new List<String>
-                {
-                    "Customer checkout failed",
-                } :
-                    new List<String>
-                {
-                    "Customer successfully checked out.",
-                };
             }
-            //using (TransactionScope t = new TransactionScope())
-            //{
-            //    dto.Reservation.NoOfDays = noOfDays == 0 ? 1 : noOfDays;
-            //    dto.Reservation.Status = RoomRsvFac.Status.CheckOut;
-            //    dto.Reservation.IsBackDateEntry = true;
-            //    RoomRsvFac.Server roomReserver = new RoomRsvFac.Server(new RoomRsvFac.FormDto
-            //    {
-            //        Dto = dto.Reservation
-            //    });
-            //    //roomReserver.RegisterArtifactObserver();//Artifact is missing. Need to attach that in Document
-            //    roomReserver.Change();
-
-            //    RoomChkCrys.Data checkinData = new RoomChkCrys.Data
-            //    {
-            //        Id = dto.Id,
-            //        Status = new ActCrys.Status.Data
-            //        {
-            //            Id = (Int64)RoomRsvFac.Status.CheckOut,
-            //        },
-            //    };
-            //    ReturnObject<Boolean> ret = (new RoomChkCrys.Server(checkinData) as Crystal.Activity.Component.IActivity).Complete();
-            //    if (this.IsError = ret.Value)
-            //    {
-            //        this.DisplayMessageList = new List<String>
-            //        {
-            //            "Customer checkout failed",
-            //        };
-            //    }
-            //    else
-            //    {
-            //        t.Complete();
-            //        dto.Status = RoomRsvFac.Status.CheckOut;
-            //        dto.CheckOutTime = checkinData.CompletionTime;
-            //        this.DisplayMessageList = new List<String>
-            //        {
-            //            "Customer successfully checked out.",
-            //        };
-            //    }
-            //}  
         }
 
         //ReturnObject<bool> ICheckIn.PaymentInsert(InvFac.FormDto invoiceFormDto, Table currentUser, ArtfFac.Dto artifactDto)
