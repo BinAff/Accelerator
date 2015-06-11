6ff51b670f78981964a21d5db889ce777177c10a
Arpan Kar <arpan.kar@binaryaffairs.com>
2015-06-11 17:27:55 +0530

    Changing table panel

--- Project/Retinue/Source Code/Retinue/Customer/Retinue.Customer.WinForm/CustomerSummary.Designer.cs
+++ Project/Retinue/Source Code/Retinue/Customer/Retinue.Customer.WinForm/CustomerSummary.Designer.cs
@@ -54,7 +54,7 @@
             this.label7.Dock = System.Windows.Forms.DockStyle.Fill;
             this.label7.Location = new System.Drawing.Point(3, 73);
             this.label7.Name = "label7";
-            this.label7.Size = new System.Drawing.Size(139, 95);
+            this.label7.Size = new System.Drawing.Size(161, 95);
             this.label7.TabIndex = 82;
             this.label7.Text = "Address";
             // 
@@ -63,9 +63,9 @@
             this.lstContact.BackColor = System.Drawing.SystemColors.Control;
             this.lstContact.Dock = System.Windows.Forms.DockStyle.Fill;
             this.lstContact.FormattingEnabled = true;
-            this.lstContact.Location = new System.Drawing.Point(168, 27);
+            this.lstContact.Location = new System.Drawing.Point(197, 27);
             this.lstContact.Name = "lstContact";
-            this.lstContact.Size = new System.Drawing.Size(244, 43);
+            this.lstContact.Size = new System.Drawing.Size(357, 43);
             this.lstContact.TabIndex = 999;
             this.lstContact.TabStop = false;
             // 
@@ -74,17 +74,17 @@
             this.label5.Dock = System.Windows.Forms.DockStyle.Fill;
             this.label5.Location = new System.Drawing.Point(3, 24);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(139, 49);
+            this.label5.Size = new System.Drawing.Size(161, 49);
             this.label5.TabIndex = 80;
             this.label5.Text = "Contact Numbers";
             // 
             // txtName
             // 
             this.txtName.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.txtName.Location = new System.Drawing.Point(168, 3);
+            this.txtName.Location = new System.Drawing.Point(197, 3);
             this.txtName.Name = "txtName";
             this.txtName.ReadOnly = true;
-            this.txtName.Size = new System.Drawing.Size(244, 20);
+            this.txtName.Size = new System.Drawing.Size(357, 20);
             this.txtName.TabIndex = 999;
             this.txtName.TabStop = false;
             // 
@@ -93,37 +93,37 @@
             this.lblName.Dock = System.Windows.Forms.DockStyle.Fill;
             this.lblName.Location = new System.Drawing.Point(3, 0);
             this.lblName.Name = "lblName";
-            this.lblName.Size = new System.Drawing.Size(139, 24);
+            this.lblName.Size = new System.Drawing.Size(161, 24);
             this.lblName.TabIndex = 22;
             this.lblName.Text = "Name";
             // 
             // txtAdds
             // 
             this.txtAdds.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.txtAdds.Location = new System.Drawing.Point(168, 76);
+            this.txtAdds.Location = new System.Drawing.Point(197, 76);
             this.txtAdds.Multiline = true;
             this.txtAdds.Name = "txtAdds";
             this.txtAdds.ReadOnly = true;
-            this.txtAdds.Size = new System.Drawing.Size(244, 89);
+            this.txtAdds.Size = new System.Drawing.Size(357, 89);
             this.txtAdds.TabIndex = 999;
             this.txtAdds.TabStop = false;
             // 
             // txtEmail
             // 
             this.txtEmail.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.txtEmail.Location = new System.Drawing.Point(168, 171);
+            this.txtEmail.Location = new System.Drawing.Point(197, 171);
             this.txtEmail.Name = "txtEmail";
             this.txtEmail.ReadOnly = true;
-            this.txtEmail.Size = new System.Drawing.Size(244, 20);
+            this.txtEmail.Size = new System.Drawing.Size(357, 20);
             this.txtEmail.TabIndex = 999;
             this.txtEmail.TabStop = false;
             // 
             // tpnlContainer
             // 
             this.tpnlContainer.ColumnCount = 3;
-            this.tpnlContainer.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 35F));
+            this.tpnlContainer.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 30F));
             this.tpnlContainer.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 5F));
-            this.tpnlContainer.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 60F));
+            this.tpnlContainer.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 65F));
             this.tpnlContainer.Controls.Add(this.txtEmail, 2, 3);
             this.tpnlContainer.Controls.Add(this.lblName, 0, 0);
             this.tpnlContainer.Controls.Add(this.label8, 0, 3);
@@ -140,8 +140,7 @@
             this.tpnlContainer.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 49F));
             this.tpnlContainer.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 95F));
             this.tpnlContainer.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 24F));
-            this.tpnlContainer.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
-            this.tpnlContainer.Size = new System.Drawing.Size(415, 196);
+            this.tpnlContainer.Size = new System.Drawing.Size(557, 196);
             this.tpnlContainer.TabIndex = 2;
             // 
             // CustomerSummary
@@ -150,7 +149,7 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.Controls.Add(this.tpnlContainer);
             this.Name = "CustomerSummary";
-            this.Size = new System.Drawing.Size(415, 196);
+            this.Size = new System.Drawing.Size(557, 196);
             this.tpnlContainer.ResumeLayout(false);
             this.tpnlContainer.PerformLayout();
             this.ResumeLayout(false);
